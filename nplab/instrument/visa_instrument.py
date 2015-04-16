562f6d43bfc7c0814a710fd89cb0f183dfbd73d7
Alan Sanders <as2180@cam.ac.uk>
2015-04-16 11:59:33 +0100

    VISA instrument class created
    VISA instrument class created to wrap VISA devices as a subclass of
    Instrument. The class is particularly bare since methods are expected
    to use the pyVISA methods in the Resource object. The Serial instrument
    class is also updated to include a super statement in the __init__
    method.

---
+++ nplab/instrument/visa_instrument.py
@@ -0,0 +1,32 @@
+__author__ = 'alansanders'
+
+from nplab.instrument import Instrument
+import visa
+
+
+class VisaInstrument(Instrument):
+    """
+    An instrument primarily using VISA communications
+    """
+
+    def __init__(self, address, settings={}):
+        """
+        :param address: VISA address as a string
+        :param settings: dictionary of instrument settings, including:
+            'read_termination', 'write_termination', 'timeout' (0 for inf),
+            'send_end' (not recommended to remove end of line character),
+            delay (time between write and read during query)
+        :type object
+        """
+        super(VisaInstrument, self).__init__()
+        rm = visa.ResourceManager()
+        assert address in rm.list_resources(), "The instrument was not found"
+        self.instr = rm.open_resource(address, **settings)
+        self._address = address
+        self._settings = settings
+
+    def __del__(self):
+        try:
+            self.instr.close()
+        except Exception as e:
+            print "The serial port didn't close cleanly:", e
