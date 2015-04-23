618d2472ecb711ba63f7b0f81cba0e9296059614
Alan Sanders <as2180@cam.ac.uk>
2015-04-23 13:27:23 +0100

    Updated QT and Traits matplotlib implementations
    All GUI imports now come from nplab.utils.gui and the traits_mpl_qt
    methods now depend on these. There are still some API issues but seems
    to work on Spyder with API 2.

--- nplab/utils/gui.py
+++ nplab/utils/gui.py
@@ -5,8 +5,24 @@ GUI Utilities
 Various utility functions for GUI-related stuff.
 """
 
+import sip
+API_NAMES = ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl", "QVariant"]
+API_VERSION = 2
+for name in API_NAMES:
+    sip.setapi(name, API_VERSION)
+
 from pyface.qt import QtCore as qt
 from pyface.qt import QtGui as qtgui
+from pyface.qt import QtCore, QtGui
+
+try:
+    assert QtCore.Signal
+    assert QtCore.Slot
+except AssertionError:
+    # if Signal and Slot don't exist, we're probably using the old API, so work around.
+    QtCore.Signal = QtCore.pyqtSignal
+    QtCore.Slot = QtCore.pyqtSlot
+
 
 def get_qt_app():
     """Retrieve or create the QApplication instance.
--- nplab/utils/traitsui_mpl_qt.py
+++ nplab/utils/traitsui_mpl_qt.py
@@ -11,7 +11,7 @@ http://markmail.org/message/z3hnoqruk56g2bje
 adapted and tested to work with PySide from Anaconda in March 2014
 """
 
-from pyface.qt import QtGui, QtCore
+from nplab.utils.gui import *
 
 import matplotlib
 # We want matplotlib to use a QT backend
@@ -23,69 +23,88 @@ from traits.api import Any, Instance
 from traitsui.qt4.editor import Editor
 from traitsui.qt4.basic_editor_factory import BasicEditorFactory
 
+
+class FigureCanvasWithDeferredDraw(FigureCanvas):
+    # This class allows us to use Qt's event loop to draw the canvas from
+    # the GUI thread, even if the call comes from outside the GUI thread.
+    # this is necessary if you want to plot from a background thread.
+    ask_for_redraw = QtCore.Signal()
+    
+    def __init__(self, figure):
+        FigureCanvas.__init__(self, figure)
+        # We connect the ask_for_redraw signal to the FigureCanvas's draw() method.
+        # using a QueuedConnection ensures that draw() is correctly called in the
+        # application's main GUI thread.
+        self.ask_for_redraw.connect(self.draw, type=QtCore.Qt.QueuedConnection)
+
+    def draw_in_main_loop(self):
+        """Draw the canvas, but do so in the Qt main loop to avoid threading nasties."""
+        self.ask_for_redraw.emit()
+
+
 class _MPLFigureEditor(Editor):
 
-   scrollable  = True
+    scrollable = True
 
-   def init(self, parent):
-       self.control = self._create_canvas(parent)
-       self.set_tooltip()
+    def init(self, parent):
+        self.control = self._create_canvas(parent)
+        self.set_tooltip()
 
-   def update_editor(self):
-       pass
+    def update_editor(self):
+        pass
 
-   def _create_canvas(self, parent):
-       """ Create the MPL canvas. """
-       # matplotlib commands to create a canvas
-       mpl_canvas = FigureCanvas(self.value)
-       return mpl_canvas
+    def _create_canvas(self, parent):
+        """ Create the MPL canvas. """
+        # matplotlib commands to create a canvas
+        mpl_canvas = FigureCanvasWithDeferredDraw(self.value)
+        return mpl_canvas
+
 
 class MPLFigureEditor(BasicEditorFactory):
-
-   klass = _MPLFigureEditor
+    klass = _MPLFigureEditor
 
 
 if __name__ == "__main__":
-   # Create a window to demo the editor
-   from traits.api import HasTraits, Int, Float, on_trait_change
-   from traitsui.api import View, Item
-   from numpy import sin, cos, linspace, pi
+    # Create a window to demo the editor
+    from traits.api import HasTraits, Int, Float, on_trait_change
+    from traitsui.api import View, Item
+    from numpy import sin, cos, linspace, pi
 
-   class Test(HasTraits):
+    class Test(HasTraits):
 
-       figure = Instance(Figure, ())
-       n = Int(11)
-       a = Float(0.5)
+        figure = Instance(Figure, ())
+        n = Int(11)
+        a = Float(0.5)
 
-       view = View(Item('figure', editor=MPLFigureEditor(),
-                               show_label=False),
-                   Item('n'),
-                   Item('a'),
-                       width=400,
-                       height=300,
-                       resizable=True)
+        view = View(Item('figure', editor=MPLFigureEditor(),
+                         show_label=False),
+                    Item('n'),
+                    Item('a'),
+                    width=400,
+                    height=300,
+                    resizable=True)
 
-       def __init__(self):
-           super(Test, self).__init__()
-           axes = self.figure.add_subplot(111)
-           self._t = linspace(0, 2*pi, 200)
-           self.plot()
-       
-       @on_trait_change('n,a')
-       def plot(self):
-           t = self._t
-           a = self.a
-           n = self.n
-           axes = self.figure.axes[0]
-           if not axes.lines:
-                axes.plot(sin(t)*(1+a*cos(n*t)), cos(t)*(1+a*cos(n*t)))
-           else:
+        def __init__(self):
+            super(Test, self).__init__()
+            axes = self.figure.add_subplot(111)
+            self._t = linspace(0, 2 * pi, 200)
+            self.plot()
+
+        @on_trait_change('n,a')
+        def plot(self):
+            t = self._t
+            a = self.a
+            n = self.n
+            axes = self.figure.axes[0]
+            if not axes.lines:
+                axes.plot(sin(t) * (1 + a * cos(n * t)), cos(t) * (1 + a * cos(n * t)))
+            else:
                 l = axes.lines[0]
-                l.set_xdata(sin(t)*(1+a*cos(n*t)))
-                l.set_ydata(cos(t)*(1+a*cos(n*t)))
-           canvas = self.figure.canvas
-           if canvas is not None:
-              canvas.draw()
-   
-   t = Test()
-   t.configure_traits()
+                l.set_xdata(sin(t) * (1 + a * cos(n * t)))
+                l.set_ydata(cos(t) * (1 + a * cos(n * t)))
+            canvas = self.figure.canvas
+            if canvas is not None:
+                canvas.draw()
+
+    t = Test()
+    t.configure_traits()
