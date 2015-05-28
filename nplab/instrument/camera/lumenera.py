fc23f06183b54756ff870eea704fe069fe1f0fd9
Richard Bowman <richard.bowman@cantab.net>
2015-05-28 18:56:40 +0100

    Lumenera Infinity camera support
    instrument.camera.lumenera now works, more or less identically to the
    opencv version.

--- nplab/instrument/camera/lumenera.py
+++ nplab/instrument/camera/lumenera.py
@@ -4,7 +4,9 @@ Lumenera camera wrapper for NPLab
 =================================
 
 This module wraps the lucam script from the legendary Christoph Gohlke 
-<http://www.lfd.uci.edu/~gohlke/>
+<http://www.lfd.uci.edu/~gohlke/> and depends on having the lucam DLL installed
+(most easily achieved by installing the Infinity Capture or Infinity Driver 
+software from Lumenera's website).
 
 @author: Richard Bowman (rwb27@cam.ac.uk)
 """
@@ -16,6 +18,13 @@ import numpy as np
 from nplab.utils.gui import qt, qtgui
 
 import nplab.instrument.camera
+
+import traits
+from traits.api import HasTraits, Property, Instance, Float, String, Button, Bool, on_trait_change
+import traitsui
+from traitsui.api import View, Item, HGroup, VGroup
+from traitsui.table_column import ObjectColumn
+from enable.component_editor import ComponentEditor
 
 import ctypes
 asVoidPtr = ctypes.pythonapi.PyCObject_AsVoidPtr #this function converts PyCObject to void *, why is it not in ctypes natively...?
@@ -32,7 +41,6 @@ Make sure you have installed the Infinity drivers (included with Infinity
 Capture), and that its version matches your Python architecture (64 or 32 bit).
 """
     try:
-        import traitsui
         import traitsui.message
         traitsui.message.error(explanation,"Infinity Driver Missing", buttons=["OK"])
     except Exception as e:
@@ -45,63 +53,146 @@ Capture), and that its version matches your Python architecture (64 or 32 bit).
 from nplab.instrument.camera import Camera, CameraParameter, ImageClickTool
 
 class LumeneraCameraParameter(CameraParameter):
+    value = Property(Float(np.NaN))
+    name = String()
+    
     def __init__(self, parent, parameter_name):
-        self._cap = parent.cap
-        self.name = parameter_name.title().replace('_',' ')
+        self.parent = parent
+        self.name = parameter_name.title().replace('_',' ') #format name prettily
         try:
-            self._parameter_ID = getattr(cv2.cv,'CV_CAP_PROP_'+parameter_name.upper().replace(' ','_'))
+            self._parameter_name = parameter_name
+            assert parameter_name in parent.parameter_names()
         except AttributeError:
             raise AttributeError("%s is not a valid capture property, try CameraParameter.list_names()")
             
     def _get_value(self):
-        return self._cap.get(self._parameter_ID)
+        return self.parent.cam.GetProperty(self._parameter_name)
         
     def _set_value(self, value):
-        return self._cap.set(self._parameter_ID, value)
+        return self.parent.cam.SetProperty(self._parameter_name, value)
         
            
 class LumeneraCamera(Camera):
-    def __init__(self,capturedevice=0):
-        self.cap=cv2.VideoCapture(capturedevice)
+    last_frame_time = -1
+    fps = -1
+    latest_frame = traits.trait_numeric.Array(dtype=np.uint8,shape=(None, None, 3))
+
+    edit_properties = Button
+    edit_video_properties = Button    
+    
+    traits_view = View(VGroup(
+                Item(name="image_plot",editor=ComponentEditor(),show_label=False,springy=True),
+                HGroup(
+                    VGroup(
+                        Item(name="take_snapshot",show_label=False),
+                        Item(name="edit_properties",show_label=False),
+                        Item(name="edit_video_properties",show_label=False),
+                        HGroup(Item(name="live_view")), #the hgroup is a trick to make the column narrower
+                    springy=False),
+                    Item(name="parameters",show_label=False,springy=True,
+                         editor=traitsui.api.TableEditor(columns=
+                             [ObjectColumn(name="name", editable=False),
+                              ObjectColumn(name="value")])),
+                springy=True),
+            layout="split"), kind="live",resizable=True,width=500,height=600,title="Camera")
+    def __init__(self,camera_number=1):
+        self.cam = lucam.Lucam(camera_number) #lucam is 1-indexed...
+        self._cameraIsStreaming = False
         
-        super(OpenCVCamera,self).__init__() #NB this comes after setting up the hardware
+        super(LumeneraCamera,self).__init__() #NB this comes after setting up the hardware
      
         
     def close(self):
         """Stop communication with the camera and allow it to be re-used."""
         super(LumeneraCamera, self).close()
-        self.cap.release()
+        self.cam.CameraClose()
         
-    def raw_snapshot(self, suppress_errors = False):
-        """Take a snapshot and return it.  Bypass filters etc."""
+    def raw_snapshot(self, suppress_errors = False, video_priority = True):
+        """Take a snapshot and return it.  Bypass filters etc.
+        
+        If video_priority is specified, don't interrupt video streaming and
+        just return the latest frame.  If it's set to false, stop the video
+        stream, take a snapshot, and re-start the video stream."""
+        
+        if self._cameraIsStreaming and video_priority:
+            #TODO add logic that waits for the next frame
+            return True, self.latest_frame #if we're streaming video, just use the latest frame
+        
         with self.acquisition_lock:
             for i in range(10):
                 try:
-                    ret, frame = self.cap.read()
-                    assert ret, "Failed to capture a frame"
-                    return ret, frame
+                    frame = self.cam.TakeSnapshot()
+                    assert frame is not None, "Failed to capture a frame"
+                    f = self.cam.GetFormat()[0]
+                    w, h = f.width // (f.binningX * f.subSampleX), f.height // (f.binningY * f.subSampleY)
+                    assert np.product(frame.shape) == w*h, "Frame was the wrong size"
+                    frame_pointer = frame.ctypes.data_as(ctypes.POINTER(ctypes.c_byte))
+                    return True, self.cam.ConvertFrameToRgb24(f, frame_pointer)
                 except Exception as e:
-                    print "Attempt number {0} failed to capture a frame from the camera!".format(i)
-                    exception = e
+                    print "Attempt number {0} failed to capture a frame from the camera: {1}".format(i,e)
         print "Camera.raw_snapshot() has failed to capture a frame."
         if not suppress_errors:
             raise IOError("Dropped too many frames from camera :(")
         else:
             return False, None
         
+    def _streamingCallback(self, context, frame_pointer, frame_size):
+        now = time.clock()
+        self.fps = 1/(now - self.last_frame_time)
+        self.last_frame_time = now
+        f = self.cam.GetFormat()[0]
+        w, h = f.width // (f.binningX * f.subSampleX), f.height // (f.binningY * f.subSampleY)
+        if frame_size == w*h:
+            #last_frame = np.ctypeslib.as_array(frame_pointer, shape=(h, w)).astype(np.ubyte, copy=True)
+            self.latest_frame = self.cam.ConvertFrameToRgb24(f, frame_pointer)
+        else:
+            print "invalid frame size"        
+    def start_streaming(self):
+        """Start streaming video from the camera as a preview."""
+        self.cam.StreamVideoControl('start_streaming')
+       # time.sleep(0.5)
+        self._callback_id = self.cam.AddStreamingCallback(self._streamingCallback)
+        self._cameraIsStreaming = True
+    def stop_streaming(self):
+        """Stop streaming video from the camera."""
+        self.cam.StreamVideoControl('stop_streaming')
+       # time.sleep(0.5)
+        self.cam.RemoveStreamingCallback(self._callback_id)
+        self._cameraIsStreaming = False
+        
     def parameter_names(self):
-        return [name.replace("CV_CAP_PROP_","") for name in dir(cv2.cv) if "CV_CAP_PROP_" in name ]
+        return lucam.Lucam.PROPERTY.keys()
     
     def initialise_parameters(self):
-        self.parameters = [OpenCVCameraParameter(self,n) for n in self.parameter_names()]
-        
-    def color_image(self):
-        """Get a colour image (bypass filtering, etc.)"""
-        ret, frame = self.raw_snapshot()
-        return cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        
-    def gray_image(self):
-        """Get a colour image (bypass filtering, etc.)"""
-        ret, frame = self.raw_snapshot()
-        return cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        
+        self.parameters = [LumeneraCameraParameter(self,n) for n in self.parameter_names()]
+    
+    def _edit_properties_fired(self):
+        self.cam.DisplayPropertyPage(None)
+    def _edit_video_properties_fired(self):
+        self.cam.DisplayVideoFormatPage(None)
+    
+    def _live_view_changed(self):
+        """Turn live view on and off"""
+        if self.live_view==True:
+            self.start_streaming()
+        else:
+            self.stop_streaming()
+#            print "starting live view thread"
+#            try:
+#                self._live_view_stop_event = threading.Event()
+#                self._live_view_thread = threading.Thread(target=self._live_view_function)
+#                self._live_view_thread.start()
+#            except AttributeError as e: #if any of the attributes aren't there
+#                print "Error:", e
+#        else:
+#            print "stopping live view thread"
+#            try:
+#                self._live_view_stop_event.set()
+#                self._live_view_thread.join()
+#                del(self._live_view_stop_event, self._live_view_thread)
+#            except AttributeError:
+#                raise Exception("Tried to stop live view but it doesn't appear to be running!")
+#    def _live_view_function(self):
+#        """this function should only EVER be executed by _live_view_changed."""
+#        while not self._live_view_stop_event.wait(timeout=0.1):
+#            self.update_latest_frame()
--- tests/test_infinity_camera.py
+++ tests/test_infinity_camera.py
@@ -3,10 +3,10 @@ import nplab.instrument.camera.lumenera as lumenera
 
 if __name__ == '__main__':
     device = int(input("Enter the number of the camera to use: "))
-    cam = lumenera.Infinity(device)
+    cam = lumenera.LumeneraCamera(device)
     cam.live_view = True
     cam.configure_traits()
     cam.live_view = False
-    cam.cap.release()
+    cam.close()
 
 1
