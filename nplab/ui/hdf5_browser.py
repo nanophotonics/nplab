a2db5fa3a792ffc6c17d5dc905a5c7769d8c63bf
William Michael Deacon <wmd22@cam.ac.uk>
2015-11-16 17:44:40 +0000

    Updated the data browser to do many things

--- nplab/ui/hdf5_browser.py
+++ nplab/ui/hdf5_browser.py
@@ -6,13 +6,19 @@ __author__ = 'alansanders'
 
 from nplab.utils.gui import *
 from PyQt4 import uic
+from PyQt4 import QtGui, QtCore
 import matplotlib
+import numpy as np
+import h5py
 
 matplotlib.use('Qt4Agg')
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from data_renderers import suitable_renderers
+from nplab.ui.data_renderers import suitable_renderers
 from nplab.ui.ui_tools import UiTools
+
+import subprocess
+import os
 
 
 # base, widget = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'hdf5_browser.ui'))
@@ -34,12 +40,19 @@ class HDF5Browser(QtGui.QWidget, UiTools):
         except AttributeError:
             self.root_name = self.f.file.filename
         self.setWindowTitle(self.root_name)
-        self.addItems(self.treeWidget.invisibleRootItem())
+        
+        self.addItems(self.treeWidget.invisibleRootItem())   
         self.treeWidget.itemClicked.connect(self.on_click)
         self.treeWidget.customContextMenuRequested.connect(self.context_menu)
+        self.treeWidget.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+        
         self.refreshButton.clicked.connect(self.refresh)
+        self.CopyButton.clicked.connect(self.CopyActivated)
+        self.clipboard = QtGui.QApplication.clipboard()
+        
         self.figureWidget = self.replace_widget(self.figureWidgetContainer, self.figureWidget, FigureCanvas(self.fig))
-
+        self.figureWidget.setMinimumWidth(800)
+        self.rendererselection.activated[str].connect(self.RenderSelectorActivated) 
     def __del__(self):
         pass  # self.f.close()
 
@@ -69,28 +82,76 @@ class HDF5Browser(QtGui.QWidget, UiTools):
     def refresh(self):
         self.treeWidget.clear()
         self.addItems(self.treeWidget.invisibleRootItem())
-
+        
+  
     def context_menu(self, position):
         menu = QtGui.QMenu()
         actions = {}
-        for operation in ['Move', 'Delete', 'Rename', 'Refresh']:
+        
+        for operation in ['Open in Igor']:
             actions[operation] = menu.addAction(operation)
         action = menu.exec_(self.treeWidget.viewport().mapToGlobal(position))
-        if action == actions['Move']:
-            print 'move'
-        elif action == actions['Delete']:
-            print 'delete'
-        elif action == actions['Rename']:
-            print 'rename'
-        elif action == actions['Refresh']:
-            self.refresh()
+
+        if action == actions['Open in Igor']:
+            self.igorOpen()
 
     def on_click(self, item, column):
         item.setExpanded(True)
-        h5object = item.data(column, QtCore.Qt.UserRole)
-        best_renderer = suitable_renderers(h5object)[0]
-        self.figureWidget = self.replace_widget(self.figureWidgetContainer, self.figureWidget, best_renderer(h5object, self))
+        if len(self.treeWidget.selectedItems())>1: 
+            self.selected_objects = [treeitem.data(column, QtCore.Qt.UserRole) for treeitem in self.treeWidget.selectedItems() ]
+        else:
+            self.selected_objects = item.data(column, QtCore.Qt.UserRole)
+        
+        print self.treeWidget.selectedItems()
+        
+        self.possible_renderers = suitable_renderers(self.selected_objects)
+        self.figureWidget = self.replace_widget(self.figureWidgetContainer, self.figureWidget, self.possible_renderers[0](self.selected_objects, self))
+        self.possible_renderer_names = [renderers.__name__ for renderers in self.possible_renderers]
+        print self.possible_renderer_names
+        self.rendererselection.clear()
+        self.rendererselection.addItems(self.possible_renderer_names)
+        
+    def RenderSelectorActivated(self,text):
+        for renderer in self.possible_renderers:
+            if renderer.__name__ == text:
+                self.figureWidget = self.replace_widget(self.figureWidgetContainer, self.figureWidget, renderer(self.selected_objects, self))
+        
+        
+        
+    def CopyActivated(self):
+        Pixelmap = QtGui.QPixmap.grabWidget(self.figureWidget)
+        self.clipboard.setPixmap(Pixelmap)
+        
+        
+    def igorOpen(self):
+        igorpath = '"C:\\Program Files (x86)\\WaveMetrics\\Igor Pro Folder\\Igor.exe"'
+        igortmpfile = os.path.dirname(os.path.realpath(__file__))+'\Igor'
+        igortmpfile=igortmpfile.replace("\\","\\\\")
+        print igortmpfile
+        open(igortmpfile, 'w').close()
+        group = self.treeWidget.currentItem().text(0)
+        dataset_name = self.treeWidget.currentItem().text(1)
+        dataset = self.selected_objects
+        print group
+        print dataset
+        if isinstance(dataset,h5py.Dataset):
+            print dataset_name
+            dset = dataset
+            data = np.asarray(dset[...])
 
+            if data.ndim == 2:
+                from PIL import Image
+                rescaled = (2**16 / data.max() * (data - data.min())).astype(np.uint8)
+
+                im = Image.fromarray(rescaled.transpose())
+                im.save(igortmpfile+'.tif')
+
+                command='/X "ImageLoad/T=tiff/N= h5Data'+' \"'+ igortmpfile+'.tif\""'
+                subprocess.Popen(igorpath+' '+command)
+            else:
+                print dataset
+                np.savetxt(igortmpfile+'.txt', data, header=dataset_name)
+                subprocess.Popen( igorpath+' '+ igortmpfile+'.txt')
 
 if __name__ == '__main__':
     import sys, h5py, os, numpy as np
