bccf4c7eacc5fcaceda8ad563703087b4c2897ef
William Michael Deacon <wmd22@cam.ac.uk>
2016-05-10 18:56:05 +0100

    shutter_ui - fixes

--- nplab/instrument/shutter/__init__.py
+++ nplab/instrument/shutter/__init__.py
@@ -32,7 +32,8 @@ class ShutterUI(QtGui.QWidget, UiTools):
         self.shutter = shutter
         super(ShutterUI, self).__init__(parent)
         uic.loadUi(os.path.join(os.path.dirname(__file__), 'shutter.ui'), self)
-        self.state.stateChanged.connect(self.on_change)
+        self.auto_connect_by_name(controlled_object = self.shutter)
+    #    self.state.stateChanged.connect(self.on_change)
 
     def on_change(self):
         self.shutter.toggle()
