6e1c9bf836398a0b650aa7e9ed2fc50c40ad8cc8
Richard Bowman <richard.bowman@cantab.net>
2016-05-11 15:22:35 +0100

    Made Shutter more abstract and added open/close
    I’ve now separated Shutter and DummyShutter, so that it’s clearer what
    must be subclassed.

--- nplab/instrument/shutter/__init__.py
+++ nplab/instrument/shutter/__init__.py
@@ -1,9 +1,10 @@
 __author__ = 'alansanders'
 
 from nplab.instrument import Instrument
-from nplab.utils.gui import *
-from PyQt4 import uic
-from nplab.ui.ui_tools import UiTools
+from nplab.utils.gui import QtGui, QtCore, uic, get_qt_app
+from nplab.ui.ui_tools import UiTools, QuickControlBox
+import os
+from nplab.utils.notified_property import DumbNotifiedProperty, register_for_property_changes
 
 
 class Shutter(Instrument):
@@ -12,17 +13,29 @@ class Shutter(Instrument):
         super(Shutter, self).__init__()
 
     def toggle(self):
-        pass
+        """Toggle the state of the shutter."""
+        raise NotImplementedError("This shutter has no way to toggle!""")
 
     def get_state(self):
-        pass
+        """Return whether the shutter is 'Open' or 'Closed'."""
+        raise NotImplementedError("This shutter has no way to get its state!""")
 
     def set_state(self, value):
-        print value
+        """Set the shutter to be either 'Open' or 'Closed'."""
+        raise NotImplementedError("This shutter has no way to set its state!""")
+        
+    def open_shutter(self):
+        """Open the shutter."""
+        self.set_state("Open")
+    
+    def close_shutter(self):
+        """Close the shutter."""
+        self.set_state("Closed")
 
     state = property(get_state, set_state)
 
     def get_qt_ui(self):
+        """Return a graphical interface for the shutter."""
         return ShutterUI(self)
 
 
@@ -38,11 +51,40 @@ class ShutterUI(QtGui.QWidget, UiTools):
     def on_change(self):
         self.shutter.toggle()
 
+class DummyShutter(Shutter):
+    """A stub class to simulate a shutter"""
+    _open = DumbNotifiedProperty(False)
+    def __init__(self):
+        """Create a dummy shutter object"""
+        self._open = False
+        super(DummyShutter, self).__init__()
+        
+    def toggle(self):
+        """toggle the state of the shutter"""
+        self._open = not self._open
+    
+    def get_state(self):
+        """Return the state of the shutter, a string reading 'open' or 'closed'"""
+        return "Open" if self._open else "Closed"
+        
+    def set_state(self, value):
+        """Set the state of the shutter (to open or closed)"""
+        if isinstance(value, str):
+            self._open = (value.lower() == "open")
+        elif isinstance(value, bool):
+            self._open = value
+        
 
 if __name__ == '__main__':
     import sys
     app = get_qt_app()
-    shutter = Shutter()
+    shutter = DummyShutter()
+
+    state_peek = QuickControlBox(title="Internal State")
+    state_peek.add_checkbox("_open", title="Shutter Open")
+    state_peek.auto_connect_by_name(controlled_object=shutter)
+    state_peek.show()    
+    
     ui = shutter.get_qt_ui()
     ui.show()
     sys.exit(app.exec_())
