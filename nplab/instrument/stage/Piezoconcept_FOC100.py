490ff950efc32c5f150d3bc2cf1e6116410c0a6b
William Michael <wmd22@cam.ac.uk>
2016-09-29 10:12:24 +0100

    Piezo concept added to stages
    Piezo concept z collar (range 100um) added to stages

---
+++ nplab/instrument/stage/Piezoconcept_FOC100.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 01 11:52:44 2015
+
+@author: hera
+"""
+import serial
+
+import nplab.instrument.serial_instrument as si
+
+class Piezoconcept(si.SerialInstrument):
+    '''A simple class for the Piezo concept FOC100 nanopositioning system'''
+    
+    def __init__(self, port=None):
+        self.termination_character = '\n'
+        self.port_settings = {
+                    'baudrate':115200,
+                    'bytesize':serial.EIGHTBITS,
+                    'parity':serial.PARITY_NONE,
+                    'stopbits':serial.STOPBITS_ONE,
+                    'timeout':1, #wait at most one second for a response
+          #          'writeTimeout':1, #similarly, fail if writing takes >1s
+           #         'xonxoff':False, 'rtscts':False, 'dsrdtr':False,
+                    }
+        si.SerialInstrument.__init__(self,port=port)
+        self.recenter()
+        
+    def move_rel(self,value,unit="n"):
+        '''A command for relative movement, where the default units is nm'''
+        if unit == "n":
+            multiplier=1
+        if unit == "u":
+            multiplier=1E3
+            
+        if (value*multiplier+self.position) > 1E5 or (value*multiplier+self.position) < 0:
+            print "The value is out of range! 0-100 um (0-1E8 nm) (Z)"
+        elif (value*multiplier+self.position) < 1E5 and (value*multiplier+self.position) >= 0:
+            self.write("MOVRX "+str(value)+unit)
+            self.position=(value*multiplier+self.position)
+    
+    def move(self,value,unit="n"):
+        '''An absolute movement command, will print an error to the console 
+        if you moveoutside of the range(100um) default unit is nm'''
+        if unit == "n":
+            multiplier=1
+        if unit == "u":
+            multiplier=1E3
+            
+        if value*multiplier >1E5 or value*multiplier <0:
+            print "The value is out of range! 0-100 um (0-1E8 nm) (Z)"
+            
+        elif value*multiplier < 1E5 and value*multiplier >=0: 
+            self.write("MOVEX "+str(value)+unit)
+            self.position = value*multiplier
+            
+    def move_step(self,direction):
+        self.move_rel(direction*self.stepsize)
+        
+    def recenter(self):
+        ''' Moves the stage to the center position'''
+        self.move(50,unit = "u")
+        self.position = 50E3
+        
+    def INFO(self):
+        return self.query("INFOS",multiline=True,termination_line= "\n \n \n \n",timeout=.1,)
+        
+
+    
+        
+
+        
