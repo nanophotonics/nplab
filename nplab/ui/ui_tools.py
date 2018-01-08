11762f69cc568103337ad26ea83c59f0a76cb363
np-hera <as2180--hera@cam.ac.uk>
2018-01-08 22:26:56 +0000

    add spin box suffice

--- nplab/ui/ui_tools.py
+++ nplab/ui/ui_tools.py
@@ -330,7 +330,7 @@ auto_connectable_controls['plaintextedit'] = {
     }
 auto_connectable_controls['spinbox'] = {
     'qt_type': QtWidgets.QSpinBox,
-    'suffices': ["_spinbox","SpinBox","_spin"],
+    'suffices': ["_spinbox","SpinBox","_spin",'_spinBox'],
     'control_change_handler': control_change_handler(),
     'control_change_slot_name': 'valueChanged',
     'property_change_handler': property_change_handler("value", int),
