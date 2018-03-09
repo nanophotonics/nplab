71236cbcda85ec2d6d18b273ba0973fe1c1cf03d
imanyakin <ilya0manyakin@gmail.com>
2018-03-09 16:37:27 +0000

    Image processing Matts NPs

--- nplab/analysis/mjh250-image-analysis.py
+++ nplab/analysis/mjh250-image-analysis.py
@@ -4,11 +4,33 @@ from nplab import datafile
 from skimage import feature, filters
 import cv2
 import math
-FILEPATH = "R:\\3-Temporary\im354\mjh250\\2018-02-17.h5"
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
+from scipy.signal import convolve2d, correlate2d
+import scipy.misc
+from PIL import Image
+import re
+import sys
+import scipy.ndimage as ndimage
+import skimage
 
+
+FILEPATH = "/home/ilya/Desktop/2018-02-17.h5"
 FOLDERPATH = "ParticleScannerScan_2"
 datafile = datafile.DataFile(FILEPATH,mode="r")
-from scipy.signal import convolve2d
+TAG = "Raman_White_Light_0Order.*"
+
+TAGS = ["Infinity3_Bias_Image", "Infinity3_FirstBkgndWhiteLight_Image", "Infinity3_FirstWhiteLight_Image", "Infinity3_SecondWhiteLight_Image", "Infinity3_SecondWhiteLight_atBkgndLoc_Image", "Raman_Bias_0Order_int", "Raman_Bias_Spectrum_int", "Raman_Bias_Spectrum_wl", "Raman_Laser_0Order_atBkgndLoc_int", "Raman_Laser_0Order_int", "Raman_Laser_Spectrum_atBkgndLoc_int", "Raman_Laser_Spectrum_atBkgndLoc_wl", "Raman_Laser_Spectrum_int", "Raman_Laser_Spectrum_wl", "Raman_White_Light_0Order_int", "Raman_White_Light_Bkgnd_0Order_int", "Raman_White_Light_Bkgnd_Spectrum_int", "Raman_White_Light_Bkgnd_Spectrum_wl", "Raman_White_Light_Spectrum_int", "Raman_White_Light_Spectrum_wl"]
+TAG = TAGS[10]
+TAG = "Raman_Laser_0Order_int.*"
+print "TAG",TAG
+
+# for k in datafile[FOLDERPATH]["Particle_100"].keys():
+# 	print k
+
+# import sys
+# sys.exit(0)
+
 images = []
 
 x_min = 0
@@ -21,22 +43,24 @@ dy = y_max-y_min
 
 N_images = 1
 
-circle_mask = np.zeros((50,50))
-r = 50; center = (25,25)
-for i in range(circle_mask.shape[0]):
-	for j in range(circle_mask.shape[1]):
-		if (i-center[0])**2 + (j-center[1])**2 < r^2:
-			circle_mask[i][j] ==1
+feature = np.zeros((21,21))
 
-import scipy.misc
-from PIL import Image
+# from PIL import Image 
+# npmask = np.array(Image.open("NPMASK.png"))
+# npmask = npmask[:,:,0]
+# npmask[npmask == 255] = 1
+# npmask  =(2*npmask) -1 
 
-def process(input_image):
+# plt.imshow(npmask,cmap="gray")
+# plt.show()
+# import sys
+# sys.exit(0)
+def get_ellipse_contour(input_image):
 
 	output_image = input_image
 
-	print np.min(output_image)
-	print np.max(output_image)
+	# print np.min(output_image)
+	# print np.max(output_image)
 
 	output_image = (255*(output_image-np.min(output_image))/float(np.max(output_image)))
 	output_image = output_image.astype(np.uint8)
@@ -60,200 +84,377 @@ def process(input_image):
 	contours_area = [(cnt,cv2.contourArea(cnt)) for cnt in contours]
 	areas = [x[1] for x in contours_area]
 	largest_contour = [cnt for (cnt,area) in contours_area if area == max(areas) ][0]
-	
-	# hull = cv2.convexHull(largest_contour)
 
-	# drawing = np.zeros(input_image.shape, np.uint8)
-
-	# cv2.drawContours(drawing,[hull],0,(255,255,255),2)
+	# ellipse = cv2.fitEllipse(cnt)
+	# print "ELLIPSE:", ellipse
+	# cv2.ellipse(img,ellipse,(0,255,0),1)
 
 
-	# M = cv2.moments(hull)
+	hull = cv2.convexHull(largest_contour)
+	xs = hull[:,0,0]
+	ys = hull[:,0,1]
 
-	# #centroid:
-	# cx = int(M['m10']/M['m00'])
-	# cy = int(M['m01']/M['m00'])
+	return np.asarray([xs,ys]).T
 
-	# output_image = drawing
 
-	# return output_image[0:dx,0:dy],(cx,cy), largest_contour
 
-	return largest_contour
-
-from skimage.filters import gaussian
-from skimage.segmentation import active_contour
-
-def main():
+def get_images():
 	particles = datafile[FOLDERPATH].keys()
-	import re
-	import sys
-	regex = re.compile("Raman_White_Light_0Order.*")
-	count = 0
-	images = []
+	regex = re.compile(TAG)
+	#laser zero order
+	outp = []
 	for particle in particles:
 		try:
 			measurements= datafile[FOLDERPATH][particle].keys()
 			for m in measurements:
 				if regex.match(m):
-					image = datafile[FOLDERPATH][particle][m]
-					image = image[x_min:x_max,y_min:y_max]
-					count = count + 1
-					if count > N_images:
-						break
-						pass
-					images.append(image)
-
+					outp.append( datafile[FOLDERPATH][particle][m])
 		except Exception as e:
 			print e
-			# return
 			print "Skipping:", particle
-		if count > N_images:
-			break
-			pass
 
-	#get the largest contour of each image
-	# images = [images[5]]
+	return outp
 
-	
-	
-	contours = [process(image) for image in images]
-	
-	#get length of square that bound ALL images
-	side_length = 0
-	for cnt in contours:
-		x,y,w,h = cv2.boundingRect(cnt)
-		side_length = max(side_length,w,h)
-	
-	
-	#make linear image
+def get_contour_area(contour):
+	return skimage.measure.moments(contour)[0,0]
 
-	output_images = []
+
+from matplotlib.path import Path 
+def make_vertex_mask(vertices,image_shape):
+	polygonPath = Path(vertices)
+	mask = np.zeros(image_shape)
+	for i in range(image_shape[0]):
+		for j in range(image_shape[1]):
+			if polygonPath.contains_points([[j,i]]): #ordering of indices is wrong way around to usual, but works!
+				mask[i,j] = 1
+	return mask
+
+def get_bounding_box(vertices):
+	xs = vertices[:,0]
+	ys = vertices[:,1]
+
+	xmin,xmax= np.min(xs),np.max(xs)
+	ymin,ymax = np.min(ys),np.max(ys)
+
+	outp = np.asarray([[xmin,ymin],[xmin,ymax],[xmax,ymax],[xmax,ymin]])
+	return outp,[xmin,ymin,xmax,ymax]
+
+def apply_snake(image,contour):
+	#contour - acts as best guess
+	contour_xs = contour[:,0]
+	contour_ys = contour[:,1]
+	init = np.asarray([contour_xs,contour_ys]).T
+	snake = skimage.segmentation.active_contour(image, init, alpha=0.005, beta=0.1, w_line=1e6, w_edge=1e7, gamma=0.01, bc='periodic', max_px_move=1.0, max_iterations=2500, convergence=0.1)
+	snake_xs,snake_ys = snake[:,0],snake[:,1]
+	return snake
+
+def run_watershed(image,markers):
+	return skimage.segmentation.watershed(image, markers, connectivity=1, offset=None, mask=None, compactness=0, watershed_line=False)
+
+
+def kmeans(image,nsegments):
+	return skimage.segmentation.slic(image, n_segments=100, compactness=10.0, max_iter=10, sigma=0, spacing=None, multichannel=True, convert2lab=False, enforce_connectivity=False, min_size_factor=0.5, max_size_factor=3, slic_zero=False)
+
+
+def plot_multiscale(images,figname,valid = None):
+	fig, axarr = plt.subplots(2,2*len(images), figsize=(36,4))
+	for i,img in enumerate(images):
+		axarr[0][2*i].imshow(img,cmap="gray")
+		
+		for x in range(img.shape[0]):
+			xs = img[x,:]
+			axarr[1][2*i].plot(xs)
+		
+		for y in range(img.shape[1]):
+			ys = img[:,y]
+			axarr[0][2*i+1].plot(ys)
+		
+	if valid != None:
+		fig.suptitle("Matched pattern? : {0}".format(valid))
+
+	plt.savefig("{0}_pyramid".format(figname))
+	plt.close(fig)
 	
-	cut_images = []
-	smoothed_images = []
-	circle_images = []
-	snakes = []
-	output_image_cut = np.zeros((side_length,len(images)*side_length))
+	# fig.close()
+	return 
+
+
+# def circle_detect(edge_segmented, fill_segmented):
+# 	from skimage import transform, draw
+# 	hspaces = transform.hough_circle(fill_segmented, 10)
+# 	accums, cxs,cys, rads = transform.hough_circle_peaks(hspaces,[10,])
+
+# 	img = np.zeros((2*fill_segmented.shape[0],2*fill_segmented.shape[1]))
+# 	for (cx,cy,rad) in zip(cxs,cys,rads):
+# 		y,x= draw.circle_perimeter(cy,cx,rad)
+# 		img[x,y]= 1
+
+# 	fig, axarr = plt.subplots(2)
+	
+# 	axarr[0].imshow(fill_segmented)
+	
+# 	axarr[1].imshow(img)
+# 	plt.show()
+
+
+# def feature_extract(image):
+
+# 	surf = cv2.xfeatures2d.SIFT_create()
+# 	#compute keypoints and descriptors
+# 	kp,des = surf.detectAndCompute(image.astype(np.uint8),None)
+
+# 	# print surf.hessianThreshold
+# 	print "KEYPOINTS",kp
+# 	if len(kp)== 0:
+# 		print "ZERO LENGTH KP - STOP"
+# 		return 
+# 	img2 = cv2.drawKeypoints(image,kp,None(255,0,0),4)
+# 	plt.imshow(img2)
+# 	plot.show()
+
+def get_largest_binary_convex_hull(image):
+	#assume image is binary
+	return skimage.morphology.convex_hull_object(image)
+
+def get_lowest_level_set(image):
+	level_set = np.zeros(image.shape)
+	level_set[image < 0.25] = 1
+	return level_set 
+def process_image(image,figname):
+	contour = get_ellipse_contour(image)
+	ellipse = cv2.fitEllipse(contour)
+
+	bbox,boxlims = get_bounding_box(contour)
+	mask =make_vertex_mask(bbox,image.shape)
+	
+	contour_xs = contour[:,0]
+	contour_ys = contour[:,1]
+
+	masked_image = mask*image
+	masked_image = masked_image[boxlims[1]+1:min(80,boxlims[3]),boxlims[0]+1:min(80,boxlims[2])] 
+
+	k = np.sqrt(2)
+	sigma = 1.0
+	sigmas = [sigma,sigma*k**2,sigma*k**3,sigma*k**4,sigma*k**5]
+	gaussians = [skimage.filters.gaussian(masked_image,s) for s in sigmas]
+	DoG = []
+	for i in range(1,len(gaussians)):
+		DoG.append(gaussians[i]-gaussians[i-1])
+
+	print "DoGs-------------------------", len(DoG)
+	for d in DoG:
+		print d
+	DoG = [d + np.min(d) for d in DoG]
+	DoG = [d-np.min(d) for d in DoG]
+	DoG = [d/np.max(d) for d in DoG]
+
+	prod = DoG[1]*DoG[2]*DoG[3]
+	prod = prod - np.min(prod)
+	prod = prod/np.max(prod)
+
+	level_set = get_lowest_level_set(prod)
+	particle_contour = get_ellipse_contour(level_set)
+	particleMask = make_vertex_mask(particle_contour,prod.shape)
+	np_only = prod-prod*((get_largest_binary_convex_hull(level_set)-1)*-1)
+
+	convolved = skimage.feature.canny(np_only,1,0.2,0.7)#correlate2d(np_only,npmask)
+	
+	convolved = skimage.morphology.binary_closing(convolved)
+	convolved = skimage.morphology.binary_dilation(convolved)
+	
+
+	edge_segmented = skimage.measure.label(convolved)
+
+	fill_segmented = np.zeros(edge_segmented.shape)
+	fill_segmented[edge_segmented==0] = 1
+	fill_segmented = skimage.measure.label(fill_segmented)
+
+	fill_segments = separate_segments(fill_segmented)
+
+	# edge_segments = separate_segments(edge_segmented)
+	# for i in range(len(edge_segments)):
+		
+	# 	(cx,cy) = CoM_image(edge_segments[i])
+	# 	axarr[0][i].imshow(edge_segments[i],cmap="gray")
+	# 	cv2.circle(edge_segments[i],(cx,cy),1,(255,255,0),1)
+		
+	def valid_segment(img, cx,cy,max_radius=5):
+		for x in range(img.shape[0]):
+			for y in range(img.shape[1]):
+				if img[x,y] > 0:
+					if  (x - cx)**2 + (y-cy)**2 < max_radius**2:
+						pass 
+					else:
+						return False
+		return True
+
+	def nonzero_pixels(image):
+		count = 0
+		for i in range(image.shape[0]):
+			for j in range(image.shape[1]):
+				if image[i,j] > 0 :
+					count = count + 1
+		return count
+
+	def has_valid_segment(image,max_radius = 5):
+		separated = separate_segments(image)
+		valid_segments = []
+
+		for i in range(len(separated)):
+			img = separated[i]
+			(cx,cy) = CoM_image(img)
+			if valid_segment(img,cx,cy,max_radius) and nonzero_pixels(img)>4:
+				ys,xs = draw.circle_perimeter(cy,cx, max_radius)
+				for (x,y) in zip(xs,ys):
+					if (x >=0 and x < img.shape[0]) and (y >=0 and y < img.shape[1]):
+						img[x,y] = 1
+				# img[xs,ys] = 1 
+				valid_segments.append(img)
+
+		return valid_segments
+
+	filled_valid_segments = has_valid_segment(fill_segmented, 5)
+	edge_valid_segments = has_valid_segment(edge_segmented,8)
+
+	if len(filled_valid_segments) > 0:
+		filled_valid = np.sum(np.asarray(filled_valid_segments),axis=0)
+	else:
+		filled_valid = np.zeros(masked_image.shape)
+
+	if len(edge_valid_segments) > 0:
+		edge_valid = np.sum(np.asarray(edge_valid_segments),axis=0)
+	else:
+		edge_valid = np.zeros(masked_image.shape)
+
+
+
+	images = [masked_image] + DoG + [prod,np_only,edge_segmented, fill_segmented,filled_valid, edge_valid]
+	plot_multiscale(images,figname,valid="Filled: {0},Edge: {1}".format(len(filled_valid_segments) > 0, len(edge_valid_segments) > 0))
+
+	return images
+
+def zero_pad(img,xdim,ydim):
+	if img.shape[0] < xdim or img.shape[1] < ydim:
+		outp = np.zeros((xdim,ydim))
+		outp[0:img.shape[0],0:img.shape[1]] = img
+		return outp
+	elif img.shape[0] == xdim and img.shape[1] == ydim:
+		return img
+	else:
+		raise ValueErro("Failed!")
+
+def separate_segments(image):
+	image = image.astype(int)
+	image = image - np.min(image)
+	assert(np.min(image)==0)
+	segments = []*np.max(image)
+	# print segments
+
+	for i in range(np.max(image)+1):
+		x = np.zeros(image.shape)
+		x[image==i] = 1
+		segments.append(x)
+	return segments
+
+
+def CoM_image(image):
+
+	it = 0
+	jt = 0
+	num = 0
+	for i in range(image.shape[0]):
+		for j in range(image.shape[1]):
+			if image[i,j] > 0:
+				it = it + i
+				jt = jt + j
+				num = num + 1
+	# num = float(image.shape[0]*image.shape[1])
+	num = float(num)
+	cx = int(round(it/num))
+	cy = int(round(jt/num))
+
+	# M = cv2.moments(image)
+	# print M
+	# cx = int(M['m10']/float(M['m00']))
+	# cy = int(M['m01']/float(M['m00']))
+	return (cx,cy)
+
+
+from skimage import draw
+def main():
+	images = get_images()
+
+	images = [image[50:150,750:850] for image in images]
+	
+	# i=49
+	# image = images[i]
+	# test_img = process_image(image,"fig_test",-1,-1)
+	# xdim,ydim = test_img[0].shape[0],test_img[0].shape[1]
+	# xdim = image.shape[0]
+	# ydim = image.shape[1]
+	limit = len(images)
+	multiscale_images = []
+		
 	for i,image in enumerate(images):
+		if i < 2:
+			pass
+		elif i < limit:
+			output_img =process_image(image,"fig{}".format(i))
+			multiscale_images=multiscale_images + [output_img]
 
-		cnt = contours[i]
-
-		#bounding rectangle:
-		x,y,_,_ = cv2.boundingRect(cnt)
-		#bounding ellipsoid
-		ellipse = cv2.fitEllipse(cnt)
-
-
-		img = image[0:dx,0:dy]
-		cut_image = img[y:y+side_length,x:x+side_length]
-
-
-
-		#draw rectanlge and circle
-		# cv2.rectangle(img,(x,y),(x+side_length,y+side_length),(0,255,0),1)
-		# cv2.ellipse(img,ellipse,(0,255,0),1)
+	xmax = 0
+	ymax = 0
+	for imgs in multiscale_images:
+		dimx, dimy = imgs[0].shape[0],imgs[0].shape[1]
+		xmax,ymax = max(dimx,xmax),max(dimy,ymax)
+		for i in imgs:
+			assert((i.shape[0],i.shape[1])==(dimx,dimy))
 
 
-		output_images.append(img)
-		f = 1.0/9
-		cut_images.append(cut_image)
-		
+	# segmented = [ (multiscale_images[i][-2][:][:],multiscale_images[i][-1][:][:]) for i in range(len(multiscale_images))]
+
+	# # print segmented
+	# for (edge,fill) in segmented:
+	# 	edge_segments = separate_segments(edge)
+	# 	fill_segments = separate_segments(fill)
+
+	# 	print "LENGTHS",
+	# 	print len(edge_segments)
+	# 	print len(fill_segments)
+	# 	fig, axarr = plt.subplots(2,max(len(edge_segments),len(fill_segments)))
+
+	# 	for i in range(len(edge_segments)):
+			
+	# 		(cx,cy) = CoM_image(edge_segments[i])
+	# 		axarr[0][i].imshow(edge_segments[i],cmap="gray")
+	# 		cv2.circle(edge_segments[i],(cx,cy),1,(255,255,0),1)
+			
+	# 	for i in range(len(fill_segments)):
+	# 		(cx,cy) = CoM_image(fill_segments[i])
+
+	# 		img = fill_segments[i]
+	# 		max_radius = 5
+	# 		valid = True
+	# 		for x in range(img.shape[0]):
+	# 			for y in range(img.shape[1]):
+	# 				if img[x,y] > 0:
+	# 					if  (x - cx)**2 + (y-cy)**2 < max_radius**2:
+	# 						pass 
+	# 					else:
+	# 						valid = False
 
 
-		smoothed_image = convolve2d(cut_image,np.array([[f,f,f],[f,f,f],[f,f,f]]))
-		smoothed_images.append(smoothed_image)
-		
-		#-----Active contour methods-----
-		# s = np.linspace(0,2*np.pi,10)
-		# x = np.asarray([0]*10) + np.linspace(0,smoothed_image.shape[0],10) + np.asarray([smoothed_image.shape[0]]*10) 
-		# y = np.linspace(0,smoothed_image.shape[1],10)+np.asarray([smoothed_image.shape[1]*10])+np.linspace(smoothed_image.shape[1],0,10)
-		# init = np.array([x,y]).T 
 
-		N = 20
-		xs = ([0]*N)+list(np.linspace(0,smoothed_image.shape[0],N))+([smoothed_image.shape[0]]*N)+list(np.linspace(smoothed_image.shape[0],0,N))
-		ys = list(np.linspace(0,smoothed_image.shape[1],N))+(N*[smoothed_image.shape[1]])+list(np.linspace(smoothed_image.shape[1],0,N)) + ([0]*N)
- 
- 		xs = xs[5:]
- 		ys = ys[5:]
-		init = np.array([xs, ys]).T
+	# 		y,x = draw.circle_perimeter(cy,cx, max_radius)
+	# 		img[x,y] = 2 
 
-		snake = active_contour(smoothed_image,init, alpha=1, beta=10,w_edge=1,bc="periodic")
-		snakes.append(snake)
-	
-		# hull= hull.reshape(hull.shape[2],hull.shape[0])
-		# snake = active_contour(gaussian(img, 3),init, alpha=0.15, beta=10, gamma=0.001,bc="periodic")
-
-
-
-		#------Hough transform method------
-		# smoothed_image = smoothed_image-np.min(smoothed_image)
-		# smoothed_image = 255*(smoothed_image/np.max(smoothed_image))
-		# smoothed_image = smoothed_image.astype(np.uint8)
-		# cut_image = cut_image.astype(np.uint8)
-		# try:
-		# 	ccs = cv2.HoughCircles(smoothed_image,cv2.HOUGH_GRADIENT,dp=1,min_dist=0,param1=200,param2=100,minRadius=0,maxRadius=0)
-		# 	circles = np.uint16(np.around(ccs))
-		# 	for i in circles[0,:]:
-		# 	    # draw the outer circle
-		# 	    cv2.circle(smoothed_image,(i[0],i[1]),i[2],(0,255,0),1)
-		# 	    # draw the center of the circle
-		# 	    cv2.circle(smoothed_image,(i[0],i[1]),2,(0,0,255),1)
-		# 	circle_images.append(smoothed_image)
-		# except:
-		# 	print "Failed in",i
-		
-
-
-	fig,[ax1,ax2,ax3,ax4] = plt.subplots(4)
-	ax1.imshow(np.concatenate(output_images,axis=1))
-	ax2.imshow(np.concatenate(cut_images,axis=1))
-	ax3.imshow(np.concatenate(smoothed_images,axis=1))
-	ax3.plot(xs,ys,'rx')
-	ax3.plot(snakes[0][:, 0], snakes[0][:, 1], '-b', lw=3)
-	
-	plt.show()
-
-	# xs,ys = [],[]
-	# for i in range(dim):
-	# 		if i*dim + j < len(images):
-	# 			index = i*dim+j
-	# 			# print "INDEX",index, len(centroids)
-	# 			x0,y0 = side_length*i,side_length*j
-	# 			x1,y1 = side_length*(i+1),side_length*(j+1)
-	# 			img = cutouts[index]
-	# 			cnt = contours[index]
-
-	# 			#get bounding rectangle, make it into square
-				
-	# 			w,h = max(w,h),max(w,h)
-	# 			# cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-				
-
-
-	# 			output_image[x0:x1,y0:y1] = img
-
-	# 			cx,cy = y0+centroids[index][0],x0+centroids[index][1]
-
-	# 			xs.append(cx)
-	# 			ys.append(cy)
-
-	# fig, ax = plt.subplots(1,figsize=(20,20))
-	# plt.imshow(output_image,cmap="gray")
-
-	# plt.plot(xs,ys,'rx')
-
-	# print "Max image value:", np.max(output_image)
-	# print "Min image value:", np.min(output_image)
-
-	# plt.show()
-
-	# xs,ys = np.mgrid[0:output_image.shape[0],0:output_image.shape[1]]
-
-	# from mpl_toolkits.mplot3d import Axes3D
-	# fig = plt.figure()
-	# ax = fig.gca(projection='3d')
-	# ax.plot_surface(xs,ys,output_image)
-	# plt.show()
+	# 		axarr[1][i].imshow(img,cmap="gray")
+	# 		title = "Valid: {0}".format(valid)
+	# 		axarr[1][i].set_title(title)			
+	# 		# cv2.circle(fill_segments[i],(cx,cy),1,(255,255,0),1)
+	# 	plt.tight_layout()
+	# 	plt.show()
 
 
 main()
+# 
