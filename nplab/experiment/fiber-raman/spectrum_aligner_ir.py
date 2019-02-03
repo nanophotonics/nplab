3819ac452d382f451ca99b40eddebfe3dd7595c4
Ermanno <em730@cam.ac.uk>
2019-02-03 16:16:54 +0000

    Ops, spectrum aligner added

--- nplab/experiment/fiber-raman/spectrum_aligner_ir.py
+++ nplab/experiment/fiber-raman/spectrum_aligner_ir.py
@@ -8,7 +8,7 @@ def least_squares(xs,ys):
 	m,_,_,_ = np.linalg.lstsq(xs_augmented,ys)
 	return m
 
-def single_position_fit(spectra,calibration_wavelengths):
+def single_position_fit(spectra,calibration_wavelengths,center_wavelength = None,debug =0):
 	'''
 	spectra: spectra taken at same position (center wavelength) but with different incident laser wavelengths
 	calibration_wavelengths - the wavelengths of the laser
@@ -19,7 +19,15 @@ def single_position_fit(spectra,calibration_wavelengths):
 
 	#assuming that this is linear in the give pixel range
 	[gradient, offset] = least_squares(xs,ys)
+	if debug > 0:
+		fig, ax = plt.subplots(1)
+		ax.plot(xs,ys,'x',label="data")
+		ax.plot(xs,gradient*np.array(xs) + offset,label="linear fit")
+		ax.set_xlabel("Pixel index")
+		ax.set_ylabel("Wavelength [nm]")
+		ax.set_title("Pixel position vs Wavelength\n Center wavelength: {0}".format(center_wavelength))
 
+		plt.show()
 	return gradient, offset
 
 
@@ -30,7 +38,7 @@ def scan_fit(dataset,debug = 0):
 	gradients = []
 	for (center_wavelength,spectra,calibration_wavelengths) in dataset:
 		print center_wavelength,len(spectra),len(calibration_wavelengths)
-		gradient, offset = single_position_fit(spectra,calibration_wavelengths)
+		gradient, offset = single_position_fit(spectra,calibration_wavelengths,debug=debug,center_wavelength=center_wavelength)
 
 		center_wls = center_wls + [center_wavelength]
 		offsets = offsets + [offset]
@@ -40,10 +48,15 @@ def scan_fit(dataset,debug = 0):
 		print "len(offsets)", len(offsets)
 		print "len(gradients)", len(gradients)
 		fig, [ax1,ax2] = plt.subplots(2)
-		ax1.plot(center_wls,offsets)
+		ax1.plot(center_wls,offsets,'x-')
 		ax1.set_title("scan_fit: center wavelength vs wavelength offset (=$\lambda$ at pixel_index=0)")
-		ax2.plot(center_wls,gradients)
+		ax2.plot(center_wls,gradients,'x-')
+		ax1.set_xlabel("Center wavelength [nm]")
+		ax1.set_ylabel("Pixel 0 wavelength (offset) [nm]")
 		ax2.set_title("scan_fit: center wavelength vs wavelength gradient (for determining wavelength for pixel_index > 0)")
+		ax2.set_xlabel("Center wavelength [nm]")
+		ax2.set_ylabel("Wavelength increment (gradient) [nm/pixel]")
+		
 		plt.show()
 
 	def mapper(center_wavelength,pixel_index):
