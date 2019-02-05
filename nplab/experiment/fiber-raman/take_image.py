34fc11c51116283821eb861de539fe3c8878bc91
Ermanno <em730@cam.ac.uk>
2019-02-05 12:10:21 +0000

    Script for taking a single CCD image

---
+++ nplab/experiment/fiber-raman/take_image.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+from nplab.instrument.spectrometer.acton_2300i import Acton
+from nplab.instrument.camera.Picam.pixis import Pixis
+from Pacton import Pacton
+
+
+def initialize_measurement(acton_port="COM7", exposure_time = 100):
+	print "Starting.."
+
+	print "Pixis..."
+	p = Pixis(debug=1)
+
+	p.StartUp()
+	print "Acton..."
+	act = Acton(port=acton_port, debug=1)
+	print "Done..."
+	pacton = Pacton(pixis=p,acton=act)
+	print "Measuring..."
+	p.SetExposureTime(exposure_time)
+	return pacton
+
+def single_shot(acton_port="COM7",center_wavelength = 0, exposure_time = 100):
+	pacton = initialize_measurement(acton_port=acton_port,exposure_time=exposure_time)
+	img = pacton.get_image(center_wavelength,roi = None,debug=0)
+	fig, ax = plt.subplots(1)
+	ax.imshow(img)
+	plt.show()
+
+single_shot()
