7d5f043bff1cd5b3ff5a500c88fb9b8a5ac4ba03
YagoDel <yagovalleinclan@gmail.com>
2019-03-22 18:08:48 +0900

    Crosshairs now scale with the size of the image. They remain centered on the pixel instead of on the edge

--- nplab/instrument/camera/camera_scaled_roi.py
+++ nplab/instrument/camera/camera_scaled_roi.py
@@ -15,6 +15,15 @@ from functools import partial
 
 
 class CameraRoiScale(Camera):
+    """
+    Camera with two main features:
+        - Scaled axes with whatever units the user wants. Subclasses may overwrite the pos_to_unit method, or provide
+        an array (x_axis or y_axis) as a lookup table.
+
+        - ROI selection using crosshairs. Subclasses should overwrite the roi method, e.g. to set ROI in the camera hardware
+
+    This class also handles binning, and keeps the scaled axes and ROI selection unaffected by the binning
+    """
     def __init__(self):
         super(CameraRoiScale, self).__init__()
         self.x_axis = None
@@ -45,21 +54,29 @@ class CameraRoiScale(Camera):
             if self.x_axis is not None:
                 return map(partial(get_value, self.x_axis), pos)
             else:
-                return pos
+                return map(int, pos)
         elif axis == 'left':
             if self.y_axis is not None:
                 return map(partial(get_value, self.y_axis), pos)
             else:
-                return pos
+                return map(int, pos)
         else:
             raise ValueError
 
     @property
     def roi(self):
-        return (0, 0, 1, 1)
+        """
+
+        :return: 4-tuple of integers. Pixel positions xmin, xmax, ymin, ymax
+        """
+        return 0, 1, 0, 1
 
     @property
     def gui_roi(self):
+        """
+
+        :return: 4-tuple of integers. x, y positions of the two crosshairs in the preview widget
+        """
         assert len(self._preview_widgets) == 1
         for wdg in self._preview_widgets:
             lims = wdg.get_roi()
@@ -67,20 +84,20 @@ class CameraRoiScale(Camera):
 
     @property
     def binning(self):
-        return (1, 1)
-
-    @binning.setter
-    def binning(self, value):
-        return
+        return 1, 1
 
     def update_widgets(self):
         for widgt in self._preview_widgets:
             roi = self.roi
             widgt._pxl_offset = (roi[0], roi[2])
             widgt._pxl_scale = self.binning
-            # size = min(((roi[1] - roi[0])/10, (roi[3]-roi[2])/10))
-            # widgt.CrossHair1.update()
-            # widgt.CrossHair2.update()
+            setattr(widgt, '_pos_to_unit', self.pos_to_unit)
+
+            size = min(((roi[1] - roi[0])/50, (roi[3]-roi[2])/50))
+            for idx in [1, 2]:
+                xhair = getattr(widgt, 'CrossHair%d' % idx)
+                xhair._size = size
+                xhair.update()
 
             vw = widgt.ImageDisplay.getView()
             for idx, lbl in enumerate(["bottom", "left"]):
@@ -105,11 +122,15 @@ class ArbitraryAxis(pyqtgraph.AxisItem):
     def tickStrings(self, values, scale, spacing):
         values = self.pos_to_unit(values)
         spacing = np.abs(np.diff(self.pos_to_unit([0, spacing])))
-        return super(ArbitraryAxis, self).tickStrings(values, scale, spacing)
+        try:
+            returnval = super(ArbitraryAxis, self).tickStrings(values, scale, spacing)
+        except TypeError:
+            # pyqtgraph throws out a TypeError when there's no ticks. We ignore it
+            returnval = []
+        return returnval
 
 
 class Crosshair(pyqtgraph.GraphicsObject):
-    CrossHairMoved = QtCore.Signal()
     Released = QtCore.Signal()
 
     def __init__(self, color, size=5, *args):
@@ -123,28 +144,35 @@ class Crosshair(pyqtgraph.GraphicsObject):
         p.drawLine(0, -self._size, 0, self._size)
 
     def boundingRect(self):
+        """Makes a clickable rectangle around the center, which is half the size of the cross hair"""
         return QtCore.QRectF(-self._size, -self._size, 2*self._size, 2*self._size)
 
     def mouseDragEvent(self, ev):
+        # Ensures the Crosshair always remains in the center of a pixel, which makes the ROI selection easier
         ev.accept()
         if ev.isStart():
             self.startPos = self.pos()
         elif ev.isFinish():
-            rounded_pos = map(int, self.pos())
+            rounded_pos = map(lambda x: int(x) + 0.5, self.pos())
             self.setPos(*rounded_pos)
         else:
             self.setPos(self.startPos + ev.pos() - ev.buttonDownPos())
-        self.CrossHairMoved.emit()
+        self.Released.emit()
 
 
 class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
     _max_num_line_plots = 4
     update_data_signal = QtCore.Signal(np.ndarray)
 
-    def __init__(self, scale=(1, 1), offset=(0, 0), unit="pxl"):
-        QtWidgets.QWidget.__init__(self)
-
+    def __init__(self, scale=(1, 1), offset=(0, 0), units=("pxl", "pxl")):
+        # QtWidgets.QWidget.__init__(self)
+        super(DisplayWidgetRoiScale, self).__init__()
         uic.loadUi(os.path.join(os.path.dirname(__file__), 'camera_controls_scaled.ui'), self)
+
+        self.units = units
+        self._pxl_scale = scale
+        self._pxl_offset = offset
+        self.binning = (1, 1)
 
         # Create a PlotItem in which the shown axis display coordinates that are offset and scaled, without changing the
         #  underlying coordinates
@@ -158,9 +186,9 @@ class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
         self.splitter.addWidget(self.ImageDisplay)
 
         self.LineDisplay = pyqtgraph.PlotWidget()
+        self.LineDisplay.showGrid(x=True, y=True)
         self.splitter.addWidget(self.LineDisplay)
         self.splitter.setHandleWidth(10)
-        self.ImageDisplay.view.setAspectLocked(False)
         self.ImageDisplay.getHistogramWidget().gradient.restoreState(Gradients.values()[1])
         self.ImageDisplay.imageItem.setTransform(QtGui.QTransform())
 
@@ -168,22 +196,14 @@ class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
         for ii in range(self._max_num_line_plots):
             self.plot += (self.LineDisplay.plot(pen=pyqtgraph.intColor(ii, self._max_num_line_plots)),)
 
-        self.CrossHair1 = Crosshair('r')
-        self.CrossHair2 = Crosshair('g')
-        self.ImageDisplay.getView().addItem(self.CrossHair1)
-        self.ImageDisplay.getView().addItem(self.CrossHair2)
+        for idx, color in enumerate(['r', 'g']):
+            crosshair = Crosshair(color)
+            self.ImageDisplay.getView().addItem(crosshair)
+            crosshair.Released.connect(self.mouseMoved)
+            setattr(self, 'CrossHair%d' % (idx + 1), crosshair)
 
-        self.LineDisplay.showGrid(x=True, y=True)
-
-        self.CrossHair1.CrossHairMoved.connect(self.mouseMoved)
-        self.CrossHair2.CrossHairMoved.connect(self.mouseMoved)
         self.checkbox_aspectratio.stateChanged.connect(self.fix_aspect_ratio)
         self.checkbox_tools.stateChanged.connect(self.show_tools)
-
-        self.unit = unit
-        self._pxl_scale = scale
-        self._pxl_offset = offset
-        self.binning = (1, 1)
         self.splitter.setSizes([1, 0])
 
     @property
@@ -193,21 +213,16 @@ class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
     def autoLevel(self):
         return self.checkbox_autolevel.isChecked()
 
-    def ccdpxl_to_unit(self, pxl):
-        return pxl
+    def _pos_to_unit(self, pos, axis):
+        """This is overwritten in CameraScaledRoi.update_widgets, setting it to CameraScaledRoi.pos_to_unit"""
+        return pos
 
-    def guipxl_to_ccdpxl(self, pos):
-        """
-        Returns a position in the appropriate units given by _pxl_scale and _pxl_offset.
-        :param pos: QPoint
-        :return:
-        """
-        tr = QtGui.QTransform()
-        # Order is important!
-        tr.translate(*self._pxl_offset)
-        tr.scale(*self._pxl_scale)
+    def pos_to_unit(self, pos):
+        """Using the CameraScaledRoi.pos_to_unit for each of the axes"""
+        x = self._pos_to_unit([pos[0]], 'bottom')[0]
+        y = self._pos_to_unit([pos[1]], 'left')[0]
 
-        return tuple(tr.map(*pos))
+        return x, y
 
     def _rescale(self):
         """
@@ -232,25 +247,24 @@ class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
         for idx in [1, 2]:
             xhair = getattr(self, "CrossHair%d" % idx)
             pos = tuple(xhair.pos())
-            post = self.guipxl_to_ccdpxl(pos)
+            post = self.pos_to_unit(pos)
             params += tuple(map(lambda x, y: x * y, pos, self.binning))
-            params += post + (self.unit, )
+            params += post + (self.units[idx-1], )
 
         diff = np.linalg.norm(np.array(params[:2]) - np.array(params[5:7]))
-        difft = np.linalg.norm(np.array(params[2:4]) - np.array(params[7:9]))
-        params += (diff, difft, self.unit, )
+        if self.units[0] == self.units[1]:
+            difft = np.linalg.norm(np.array(params[2:4]) - np.array(params[7:9]))
+            unit = self.units[0]
+        else:
+            difft = -1
+            unit = ''
+        params += (diff, difft, unit, )
 
         self.labelCrossHairPositions.setText(
             "<span style='color: red'>Pixel: [%i,%i]px Unit: (%g, %g)%s</span>, " \
             "<span style='color: green'> Pixel: [%i,%i]px Unit: (%g, %g)%s</span>, " \
             "Delta pixel: %g px Delta Unit: %g %s"
             % params)
-        # self.labelCrossHairPositions.setText(
-        #     "<span style='color: red'>Pixel: [%i,%i]px Unit: (%g, %g)%s</span>, " \
-        #     "<span style='color: green'> Pixel: [%i,%i]px Unit: (%g, %g)%s</span>, " \
-        #     "Delta pixel: [%i,%i]px Delta Unit: (%g, %g)%s"
-        #     % (x1, y1, xu1, yu1, self.unit, x2, y2, xu2, yu2, self.unit, abs(x1 - x2), abs(y1 - y2), abs(xu1 - xu2),
-        #        abs(yu1 - yu2), self.unit))
 
     def update_image(self, newimage):
 
@@ -280,6 +294,7 @@ class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
                                        autoRange=self.autoRange,
                                        autoLevels=self.autoLevel,
                                        scale=(scale[0], scale[1]))
+        self.mouseMoved()
 
     def fix_aspect_ratio(self):
         boolean = self.checkbox_aspectratio.isChecked()
@@ -295,6 +310,7 @@ class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
             self.ImageDisplay.getHistogramWidget().hide()
             self.ImageDisplay.ui.roiBtn.hide()
             self.ImageDisplay.ui.menuBtn.hide()
+
     def get_roi(self):
         """
         Returns the cross hair positions
@@ -313,7 +329,7 @@ class DisplayWidgetRoiScale(QtWidgets.QWidget, UiTools):
         miny, maxy = map(lambda x: int(x),
                          (min(pos1[1], pos2[1]), max(pos1[1], pos2[1])))
 
-        return minx, maxx - 1, miny, maxy - 1
+        return minx, maxx, miny, maxy
 
 
 
