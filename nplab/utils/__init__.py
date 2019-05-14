bc54f8ba79c47f0375ead25efc45153f19c887fa
YagoDel <yagovalleinclan@gmail.com>
2019-05-14 12:22:01 +0900

    Monitor thread closes on exiting. Warning limit values

--- nplab/utils/__init__.py
+++ nplab/utils/__init__.py
@@ -6,7 +6,7 @@ import threading
 import collections
 
 
-def monitor_property(instance, property_name, how_long, how_often):
+def monitor_property(instance, property_name, how_long, how_often, warn_limits=None):
     """
     Given an nplab instrument instance and one of it's properties, it creates a deque containing the property's value
     over time. The deque gets updated in a background thread.
@@ -15,6 +15,7 @@ def monitor_property(instance, property_name, how_long, how_often):
     :param property_name: str
     :param how_long: float. Length of time you want to monitor for. In seconds
     :param how_often: float. Interval between measurements. In seconds
+    :param warn_limits: None or 2-tuple. If the monitored value goes outside these limits, throws out a warning.
     :return:
     """
     setattr(instance, property_name + '_history', collections.deque(maxlen=how_long / how_often))
@@ -22,8 +23,13 @@ def monitor_property(instance, property_name, how_long, how_often):
 
     def monitor():
         while getattr(instance, '_monitoring_' + property_name):
-            getattr(instance, property_name + '_history').append(getattr(instance, property_name))
+            value = getattr(instance, property_name)
+            if warn_limits is not None:
+                if value < warn_limits[0] or value > warn_limits[1]:
+                    raise ValueError('%s=%g is outside of range' % (property_name, value))
+            getattr(instance, property_name + '_history').append(value)
             time.sleep(how_often)
 
     monitor_thread = threading.Thread(target=monitor)
+    monitor_thread.setDaemon(True)  # a daemon thread will not prevent the program from exiting
     monitor_thread.start()
