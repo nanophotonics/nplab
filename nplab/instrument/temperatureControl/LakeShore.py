a33c279897893dc0ba166b81afcd8f3c9b5638bc
YagoDel <yagovalleinclan@gmail.com>
2019-05-30 07:33:07 +0900

    Debugged temperature monitoring thread

--- nplab/instrument/temperatureControl/LakeShore.py
+++ nplab/instrument/temperatureControl/LakeShore.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from nplab.instrument.visa_instrument import VisaInstrument
-from nplab.instrument.temperatureControl import TemperatureControl
+from nplab.instrument.temperatureControl import TemperatureControlMixin
 
 
-class LS331(VisaInstrument, TemperatureControl):
+class LS331(VisaInstrument, TemperatureControlMixin):
     """https://www.lakeshore.com/ObsoleteAndResearchDocs/331_Manual.pdf"""
     def __init__(self, address, **kwargs):
         super(LS331, self).__init__(address, **kwargs)
