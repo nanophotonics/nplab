08b62b3d6f2abf6589a006fcd781ad3f01e77f01
eoinell <43847943+eoinell@users.noreply.github.com>
2019-09-30 14:07:00 +0100

    minor updates

--- nplab/analysis/misc.py
+++ nplab/analysis/misc.py
@@ -96,7 +96,7 @@ def stokesratio(Stokes_counts, antiStokes_counts, shift, laser_wavelength = 785.
     
     omega_AS = omega +cnv.simple_wavelength_to_omega(laser_wavelength)
     omega_S = cnv.simple_wavelength_to_omega(laser_wavelength)-omega
-    logarg = (Stokes_counts/antiStokes_counts)*(omega_AS/omega_S)**4
+    logarg = (np.array(Stokes_counts)/np.array(antiStokes_counts))*(omega_AS/omega_S)**4
     T = scipy.constants.hbar*omega/(scipy.constants.k*np.log(logarg))
     return T        
             
@@ -179,10 +179,10 @@ class exponential3: # work in omega, more complicated exponential
 
 
 
-def get_laser_power_from_leak(power_series):
+def get_laser_power_from_leak(power_series, shifts):
     extracted_powers = []
     for spectrum in power_series:
-        cut = truncate(spectrum, power_series.attrs['wavelengths'], 784, 786)[0]
+        cut = truncate(spectrum, shifts, -15, 15)[0]
         cut =  AP.Run(cut, 1, Auto_Remove = True)
         extracted_powers.append(np.sum(cut))
     return extracted_powers
@@ -244,148 +244,7 @@ def calibrate_Si(counts, wavelengths, shift=520, centre_wl = 785.):
         print len(new_wavelengths), len(wavelengths)
         
     return new_wavelengths  
-def calibrate_BPT(counts, wavelengths, notch, analysis_range, Si_counts=None, Si_wl = None):
-    BPT_lines = [1278.49,1078.08,1012.33,995.828,827.954,758.232,709.338,655.325,615.569,545.529,478.053,406.404,290.517]    
-    Si_line = 520.7
-    
-    init_shifts = -cnv.wavelength_to_cm(wavelengths, centre_wl = 785.)
-    counts, init_shifts = truncate(counts, init_shifts, analysis_range[0], analysis_range[1])
-    for line in BPT_lines:
-        if line>max(init_shifts):
-            BPT_lines.remove(line)
-    S_portion, S_shifts = truncate(counts,init_shifts, notch, np.inf)
-    AS_portion, AS_shifts = truncate(counts,init_shifts, -np.inf, -notch )
-    
-    
-   
-    S_bg = AP.Run(S_portion, 4, Auto_Remove = False )
-    AS_bg = AP.Run(AS_portion, 4, Auto_Remove = False )
 
-    
-    full_bg = np.append(AS_bg,S_bg)
-    notch_len = len(counts) - len(full_bg)
-    full_bg = np.insert(full_bg,len(AS_portion),np.zeros(notch_len))
-    subbed_counts = counts-full_bg
-    
-    S_values, S_errors =  AFR.Run(S_portion-S_bg, S_shifts,  Noise_Threshold = 1.6)
-    AS_values, AS_errors = AFR.Run(AS_portion-AS_bg, -AS_shifts,  Noise_Threshold = 1.3)
-   
-    plt.figure('BPT calibration plot')  
-    plt.plot(init_shifts, counts)
-    #plt.plot(S_shifts, S_bg, color = 'saddlebrown')
-    plt.fill_between(S_shifts, S_bg, 0,color = 'saddlebrown', alpha = 0.2)
-    #plt.plot(AS_shifts, AS_bg, color = 'saddlebrown')
-    plt.fill_between(AS_shifts, AS_bg, 0,color = 'saddlebrown', alpha = 0.2)
-    plt.ylim(np.min(full_bg[0:100]-100), max(counts)+100)
-    plt.fill_between([-notch, notch], 0, 1000000, color = 'olive', alpha = 0.1)
-    
-    
-    S_peak_array = []
-    S_peak_height = []
-    S_peak_shift = []
-    S_peak_width = []
-    S_peak_area = []
-    if S_values is not None:
-        for peak_number in range(len(S_values)/3):
-            if S_values[peak_number*3:peak_number*3 +3][2]<15:
-                S_peak_array = S_values[peak_number*3:peak_number*3 +3]
-                S_peak_height.append( S_peak_array[0])
-                S_peak_shift.append( S_peak_array[1])
-                S_peak_width.append( S_peak_array[2])
-                
-            #arrow_start = S_peak_height+ np.float(an.truncate(full_bg, shifts, S_peak_shift, S_peak_shift+10)[0][0]) +arrow_length+100
-            #plt.arrow(S_peak_shift, arrow_start, 0, -arrow_length, width = 10, length_includes_head = True, head_width = 20, head_length = arrow_length/6) 
-        for peak_number in range(len(S_peak_height)):    
-            S_peak_area.append( 4*np.pi*S_peak_height[peak_number]*S_peak_width[peak_number])
-            L_bg, L_input = truncate(full_bg, init_shifts, S_peak_shift[peak_number]-5*S_peak_width[peak_number], S_peak_shift[peak_number]+5*S_peak_width[peak_number])
-            L_output =  AFR.L(L_input, S_peak_height[peak_number], S_peak_shift[peak_number], S_peak_width[peak_number])        
-        
-            plt.figure('BPT calibration plot')
-            plt.plot(L_input, L_output+L_bg, color = 'k')
-            plt.fill_between(L_input, L_output+L_bg, L_bg, color = 'k', alpha = 0.2)
-    
-    AS_peak_array = []
-    AS_peak_height = []
-    AS_peak_shift = []
-    AS_peak_width = []
-    AS_peak_area = []
-    if AS_values is not None:
-        for peak_number in range(len(AS_values)/3):
-            if AS_values[peak_number*3:peak_number*3 +3][2]<15:
-                AS_peak_array=AS_values[peak_number*3:peak_number*3+3]
-                AS_peak_height.append(AS_peak_array[0])
-                AS_peak_shift.append(-AS_peak_array[1])
-                AS_peak_width.append(AS_peak_array[2])
-                
-            
-            #arrow_start = S_peak_height+ np.float(an.truncate(full_bg, shifts, S_peak_shift, S_peak_shift+10)[0][0]) +arrow_length+100
-            #plt.arrow(S_peak_shift, arrow_start, 0, -arrow_length, width = 10, length_includes_head = True, head_width = 20, head_length = arrow_length/6) 
-        for peak_number in range(len(AS_peak_height)):
-            AS_peak_area.append(4*np.pi*AS_peak_height[peak_number]*AS_peak_width[peak_number])
-            L_bg, L_input = truncate(full_bg, init_shifts, AS_peak_shift[peak_number]-5*AS_peak_width[peak_number], AS_peak_shift[peak_number]+5*AS_peak_width[peak_number])
-            L_output =  AFR.L(L_input, AS_peak_height[peak_number], AS_peak_shift[peak_number], AS_peak_width[peak_number])        
-            
-            plt.figure('BPT calibration plot')
-            plt.plot(L_input, L_output+L_bg, color = 'k')
-            plt.fill_between(L_input, L_output+L_bg, L_bg, color = 'k', alpha = 0.2)
-            plt.savefig('Spectrum for calibration.png')
-        if len(S_peak_shift)!=len(AS_peak_shift):
-            print 'Peak\'s gone missing!'
-       
-        
-        S_real_peaks = []
-        for S_peak in S_peak_shift:
-            closest = find_closest(S_peak, np.array(BPT_lines))
-            if closest[2]<50:
-                S_real_peaks = np.append(S_real_peaks, closest[0])
-            else:
-                S_peak_shift.remove(S_peak)    
-        S_real_peaks = np.append(S_real_peaks, 0)
-        S_peak_shift = np.append(S_peak_shift, 0)
-        if Si_counts is not None:
-            S_wl = get_peak_heights(Si_counts, Si_wl, Si_line, Range = 50, return_wavelength = True,inputcm=True)[2]
-            Si_S_shift = -cnv.wavelength_to_cm(S_wl, centre_wl = 785.)
-            
-            S_real_peaks = np.append(S_real_peaks, 520.7)
-            S_peak_shift = np.append(S_peak_shift, Si_S_shift)
-            
-        
-        S_fit = np.polyfit(S_peak_shift, S_real_peaks, 3)
-        AS_real_peaks = []
-        for AS_peak in AS_peak_shift:
-            closest = find_closest(AS_peak, -np.array(BPT_lines))
-            if closest[2]<50:
-                AS_real_peaks = np.append(AS_real_peaks, closest[0])
-            else:
-                AS_peak_shift.remove(AS_peak)
-        
-        AS_real_peaks = np.append(AS_real_peaks, 0)
-        AS_peak_shift = np.append(AS_peak_shift, 0)   
-        if Si_counts is not None:
-            AS_wl = get_peak_heights(Si_counts, Si_wl, Si_line, Range = 50, return_wavelength = True, inputcm=True)[3]
-            Si_AS_shift = cnv.wavelength_to_cm(AS_wl, centre_wl = 785.)
-            AS_real_peaks = np.append(AS_real_peaks, -Si_line)
-            AS_peak_shift = np.append(AS_peak_shift, Si_AS_shift)
-            
-        AS_fit = np.polyfit(AS_peak_shift, AS_real_peaks, 3)
-        
-        
-
-        plt.figure()
-        plt.plot(S_peak_shift, S_real_peaks, '+', color='g', markersize=15)
-        plt.plot(AS_peak_shift, AS_real_peaks, '+', color='b', markersize=15)
-        calibrated_S_shifts = np.polyval(S_fit,truncate(init_shifts, init_shifts, 0, np.inf)[1])
-        calibrated_AS_shifts = np.polyval(AS_fit, truncate(init_shifts, init_shifts, -np.inf, 0)[1])
-        plt.plot(truncate(init_shifts, init_shifts, 0, np.inf)[1], calibrated_S_shifts, color='g', markersize=10)
-        plt.plot(truncate(init_shifts, init_shifts, -np.inf, 0)[1], calibrated_AS_shifts,color='b', markersize=10)
-        plt.grid()
-        plt.savefig('BPT calibration plot.png')
-        
-        init_shifts = -cnv.wavelength_to_cm(wavelengths, centre_wl = 785.)
-        calibrated_shifts = np.append(calibrated_AS_shifts, calibrated_S_shifts)
-        np.savetxt(r'C:\Users\Eoin Elliott\Documents\GitHub\nplab\nplab\calibration\Lab 5\785nm_Stokes_polynomial.txt', S_fit)
-        np.savetxt(r'C:\Users\Eoin Elliott\Documents\GitHub\nplab\nplab\calibration\Lab 5\785nm_anti_Stokes_polynomial.txt', AS_fit)
-        return calibrated_shifts, S_fit, AS_fit
 def transmission_function(S_fit, AS_fit):
   
     print r'C:\Users\Eoin Elliott\Documents\GitHub\nplab\nplab\calibration\Lab 5' 
@@ -444,5 +303,5 @@ if __name__ =='__main__':
     spec = power_series[-1]
     wavelengths = power_series.attrs['wavelengths']
     notch = 230.
-    calibrate_BPT(spec, wavelengths, notch, (-600, 800))
+
     
