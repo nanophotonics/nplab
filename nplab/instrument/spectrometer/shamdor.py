d2c2599e637f7f9a5425058b8da46cca09d1c211
Matthew Horton <mjh250@cam.ac.uk>
2019-12-02 12:19:01 +0000

    Added support for viewing Raman shift from gui

--- nplab/instrument/spectrometer/shamdor.py
+++ nplab/instrument/spectrometer/shamdor.py
@@ -6,16 +6,28 @@ Created on Sat Jul 08 19:47:22 2017
 """
 from nplab.instrument.camera.Andor import Andor
 from nplab.instrument.spectrometer.shamrock import Shamrock
-
+import numpy as np
 class Shamdor(Andor):
     ''' Wrapper class for the shamrock and the andor
     '''
-    def __init__(self, pixel_number = 1600):
+    def __init__(self, pixel_number = 1600, use_shifts = False, laser = '_633'):
         self.shamrock = Shamrock()
         #self.shamrock.pixel_number = pixel_number
         self.shamrock.SetNumberPixels(1600)
         self.shamrock.pixel_width = 16
+        self.use_shifts = use_shifts
+        self.laser = laser
         super(Shamdor, self).__init__()
     def get_xaxis(self):
-        return self.shamrock.GetCalibration()[::-1]
+        if self.use_shifts:
+            if self.laser == '_633': centre_wl = 632.8
+            elif self.laser == '_785': centre_wl = 784.81
+            wavelengths = np.array(self.shamrock.GetCalibration()[::-1])
+            return ( 1./(centre_wl*1e-9)- 1./(wavelengths*1e-9))/100    
+        else:
+            return self.shamrock.GetCalibration()[::-1]
+    
     x_axis = property(get_xaxis)
+    
+        
+    
