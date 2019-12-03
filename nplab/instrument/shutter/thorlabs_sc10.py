617fb81cd7c7426929768035170ddd870a4a3001
gd392 <gd392@cam.ac.uk>
2019-12-03 14:07:04 +0000

    Rewriting previous commit to try communication with shutter first, then rely on the local variable

--- nplab/instrument/shutter/thorlabs_sc10.py
+++ nplab/instrument/shutter/thorlabs_sc10.py
@@ -3,6 +3,16 @@ __author__ = 'alansanders'
 from nplab.instrument.shutter import Shutter
 import nplab.instrument.serial_instrument as serial
 
+def bool_to_state(Bool):
+    if Bool:
+        return 'Open'
+    if not Bool:
+        return 'Closed'
+def state_to_bool(state):
+    if state == 'Open':
+        return True
+    if state == 'Closed':
+        return False
 
 class ThorLabsSC10(Shutter, serial.SerialInstrument):
     port_settings = dict(baudrate=9600,
@@ -14,38 +24,62 @@ class ThorLabsSC10(Shutter, serial.SerialInstrument):
                          xonxoff=False, rtscts=False, dsrdtr=False,
                          )
     termination_character = "\r"  #: All messages to or from the instrument end with this character.
-
+    '''
+    self._state trys to keep track of the shutter state. This is because communication quite frequently fails with this shutter for some reason.
+    The toggle function always works, however self.query('ens?') often returns None, along with the message 'Command did not echo!!!'.
+    self._state may be incorrect initially, if the laser shutter is open, and communication fails. 
+    In this case, simply specify the shutter state manually with
+    >>> shutter._state = 'Open' # for example
+    
+    The self._state attribute is overwritten if communication with the shutter succeeds at any time, so this should maximise the state being right. 
+    using the buttons on the shutter will of course mess this all up. 
+    '''
     def __init__(self, port=None):
         serial.SerialInstrument.__init__(self, port=port)
         Shutter.__init__(self)
-        self.ignore_echo = True
-        self._state = False # closed
+        self.ignore_echo = True     
+        self._state = 'Closed'  # usually the case      
+        self.get_state(report_success = True) # overwrites self._state if communication succeeds     
+            
     def toggle(self):
         self.write('ens')
-        self._state = not self._state
-        
+        self._state = bool_to_state(not state_to_bool(self._state))#toggles self._state
+#        
 #    def get_state(self):
 #        if self.query('ens?') == '0':
 #            return 'Closed'
 #        elif self.query('ens?') == '1':
 #            return 'Open'
-    def get_state(self):
-        return self._state
-    def set_state(self):
-        pass
-    def open_shutter(self):
-        if not self._state:
-            self.toggle()
-        elif self._state:
-            print 'Shutter is already open!'
-        else: print 'Unknown if shutter is open or closed - plug in and out USB'
-    def close_shutter(self):  
-        if self._state:
-            self.toggle()
-        elif not self._state:
-            print 'Shutter is already closed!'
+    
+    def get_state(self, report_success = False):
+        if bool(int(self.query('ens?'))):
+            self._state = 'Open'            
+            return self._state
+        if not bool(int(self.query('ens?'))):
+            self._state = 'Closed'            
+            return self._state
         else:
-            print 'Unknown if shutter is open or closed - plug in and out USB'
+            if report_success: 
+                print '''Communication with shutter failed;  
+                Assuming shutter is closed! 
+                Change shutter._state if not!'''           
+            return self._state
+    def set_state(self, state):
+        if state_to_bool(self.get_state()) != state_to_bool(state):
+            self.toggle()
+        
+    def open_shutter(self):
+        if not state_to_bool(self.get_state()):
+            self.toggle()
+        elif state_to_bool(self._state):
+            print 'Shutter is already open!'
+        
+    def close_shutter(self):  
+        if state_to_bool(self._state):
+            self.toggle()
+        elif not state_to_bool(self._state):
+            print 'Shutter is already closed!'
+        
     def set_mode(self,n):
         """ Where n equals an associated mode
             mode=1: Sets the unit to Manual Mode
