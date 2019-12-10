0c06d863738b3da67eb643ce600d7449891dfb20
Eoin Elliott <ee306@cam.ac.uk>
2019-12-10 13:01:14 +0000

    Revert "Merge branch 'master' into python3"
    This reverts commit 99d6f34a71f557619f3aa8b7d36f7217b2f86e2e, reversing
    changes made to d76d59df948d1fc94d6ac4af2615577c7d44a5e7.

--- nplab/instrument/spectrometer/seabreeze.py
+++ nplab/instrument/spectrometer/seabreeze.py
@@ -167,18 +167,16 @@ class OceanOpticsSpectrometer(Spectrometer, Instrument):
 
     def __init__(self, index):
         """Initialise the spectrometer"""
-        
         self.index = index  # the spectrometer's ID, used by all seabreeze functions
         self._comms_lock = threading.RLock()
         self._isOpen = False
         self._open()
-              
         super(OceanOpticsSpectrometer, self).__init__()
-        self._minimum_integration_time = None        
+        self._minimum_integration_time = None
         self.integration_time = self.minimum_integration_time
         self._tec_enabled = True
         self.enable_tec = True
-        self.get_API_version() 
+
     def __del__(self):
         self._close()
         super(OceanOpticsSpectrometer, self).__del__()
@@ -213,28 +211,13 @@ class OceanOpticsSpectrometer(Spectrometer, Instrument):
         return self._config_file
 
     config_file = property(open_config_file)
-    def get_API_version(self):
-        N = 32  # make a buffer for the DLL to return a string into
-        s = ctypes.create_string_buffer(N)
-        e = ctypes.c_int()
-        try:
-            seabreeze.seabreeze_get_model(self.index, byref(e), byref(s), N)
-            self.API_ver = 2
-        except:#self.API_ver == 2:
-            seabreeze.seabreeze_get_spectrometer_type(self.index, byref(e), byref(s), N)
-            self.API_ver = 1
-        check_error(e)        
-        
-    
+
     def get_model_name(self):
         if self._model_name is None:
             N = 32  # make a buffer for the DLL to return a string into
             s = ctypes.create_string_buffer(N)
             e = ctypes.c_int()
-            if self.API_ver == 2:
-                seabreeze.seabreeze_get_model(self.index, byref(e), byref(s), N)
-            if self.API_ver == 1:
-                seabreeze.seabreeze_get_spectrometer_type(self.index, byref(e), byref(s), N)
+            seabreeze.seabreeze_get_model(self.index, byref(e), byref(s), N)
             check_error(e)
             self._model_name = str(s.value)
         return self._model_name
@@ -279,10 +262,7 @@ class OceanOpticsSpectrometer(Spectrometer, Instrument):
         e = ctypes.c_int()
         if milliseconds < self.minimum_integration_time:
             raise ValueError("Cannot set integration time below %d microseconds" % self.minimum_integration_time)
-        if self.API_ver == 1:
-            seabreeze.seabreeze_set_integration_time(self.index, byref(e), c_ulong(int(milliseconds * 1000)))
-        if self.API_ver == 2:
-            seabreeze.seabreeze_set_integration_time_microsec(self.index, byref(e), c_ulong(int(milliseconds * 1000)))
+        seabreeze.seabreeze_set_integration_time_microsec(self.index, byref(e), c_ulong(int(milliseconds * 1000)))
         check_error(e)
         self._latest_integration_time = milliseconds
 
@@ -292,10 +272,7 @@ class OceanOpticsSpectrometer(Spectrometer, Instrument):
         """Minimum allowable value for integration time"""
         if self._minimum_integration_time is None:
             e = ctypes.c_int()
-            if self.API_ver == 1:
-                min_time = seabreeze.seabreeze_get_minimum_integration_time_micros(self.index, byref(e))
-            if self.API_ver == 2:
-                min_time = seabreeze.seabreeze_get_min_integration_time_microsec(self.index, byref(e))            
+            min_time = seabreeze.seabreeze_get_min_integration_time_microsec(self.index, byref(e))
             check_error(e)
             self._minimum_integration_time = min_time / 1000.
         return self._minimum_integration_time
@@ -492,7 +469,7 @@ def main():
  #       ui = spectrometers.get_qt_ui()
  #       ui.show()
  #       sys.exit(app.exec_()) #this is the "long way" of running a GUI
-        spectrometers.show_gui(blocking = False)  # the "short way" of running a GUI
+        spectrometers.show_gui()  # the "short way" of running a GUI
     except OceanOpticsError as error:
         print("An error occurred with the spectrometer: %s" % error)
     finally:
@@ -506,6 +483,4 @@ def main():
 
 # example code:
 if __name__ == "__main__":
-#    main()
-    spec = OceanOpticsSpectrometer(0)
-    spec.show_gui(blocking = False)
+    main()
