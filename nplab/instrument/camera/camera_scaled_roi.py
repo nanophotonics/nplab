018a4a72339721f5756cafd7b5a936560e79ee75
eoinell <43847943+eoinell@users.noreply.github.com>
2019-12-16 14:48:16 +0000

    Added a dummy ROI-scaled camera

--- nplab/instrument/camera/camera_scaled_roi.py
+++ nplab/instrument/camera/camera_scaled_roi.py
@@ -12,7 +12,10 @@ from nplab.instrument.camera import Camera
 import pyqtgraph
 import numpy as np
 from pyqtgraph.graphicsItems.GradientEditorItem import Gradients
+from nplab.utils.array_with_attrs import ArrayWithAttrs
 
+from nplab.ui.ui_tools import UiTools, uic
+from weakref import WeakSet
 
 class CameraRoiScale(Camera):
     """
@@ -238,3 +241,35 @@ class DisplayWidgetRoiScale(ExtendedImageView):
                           scale=scale)
         else:
             raise ValueError('Cannot display. Array shape unrecognised')
+            
+
+class DummyCameraRoiScale(CameraRoiScale):
+    """A version of the Camera code  """
+    def __init__(self, data = 'spectrum'):
+        super(DummyCameraRoiScale, self).__init__()
+        self.data = data
+    def raw_snapshot(self, update_latest_frame = True):
+        """Returns a True, stating a succesful snapshot, followed by a (100,100)
+        picture randomly generated image"""
+        if self.data == 'spectrum':
+            ran = 100*ArrayWithAttrs(np.random.random(100))
+            ran.attrs['x-axis'] = np.arange(100)
+        else:
+            ran = 100*np.random.random((100,100))
+        self._latest_raw_frame = ran
+        return True, ran
+    def get_preview_widget(self):
+        self._logger.debug('Getting preview widget')
+        if self._preview_widgets is None:
+            self._preview_widgets = WeakSet()
+        new_widget = DisplayWidgetRoiScale()
+        self._preview_widgets.add(new_widget)
+        return new_widget   
+    
+
+
+
+if __name__ == '__main__':
+
+    dcrd = DummyCameraRoiScale()
+    dcrd.show_gui(blocking = False)
