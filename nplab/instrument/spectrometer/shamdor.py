e4e0d89bb948bd6844a08164ecbb067275fb9621
gd392 <gd392@cam.ac.uk>
2020-01-17 10:16:49 +0000

    style changes to shamdor

--- nplab/instrument/spectrometer/shamdor.py
+++ nplab/instrument/spectrometer/shamdor.py
@@ -7,17 +7,18 @@ Created on Sat Jul 08 19:47:22 2017
 from nplab.instrument.camera.Andor import Andor
 from nplab.instrument.spectrometer.shamrock import Shamrock
 import numpy as np
+
 class Shamdor(Andor):
     ''' Wrapper class for the shamrock and the andor
     '''
-    def __init__(self, pixel_number = 1600, use_shifts = False, laser = '_633'):
+    def __init__(self, pixel_number = 1600, pixel_width = 16, use_shifts = False, laser = '_633'):
         self.shamrock = Shamrock()
-        #self.shamrock.pixel_number = pixel_number
-        self.shamrock.SetNumberPixels(1600)
-        self.shamrock.pixel_width = 16
+        self.shamrock.pixel_number = pixel_number
+        self.shamrock.pixel_width = pixel_width
         self.use_shifts = use_shifts
         self.laser = laser
         super(Shamdor, self).__init__()
+    
     def get_xaxis(self):
         if self.use_shifts:
             if self.laser == '_633': centre_wl = 632.8
