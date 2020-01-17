2d99abfd2a1e2add92a77065e3ac654189de2de6
np-hera <np-hera@users.noreply.github.com>
2020-01-17 13:07:33 +0000

    Update camera_scaled_roi.py
    removing references to detector shape

--- nplab/instrument/camera/camera_scaled_roi.py
+++ nplab/instrument/camera/camera_scaled_roi.py
@@ -80,7 +80,7 @@ class CameraRoiScale(Camera):
         assert len(self._preview_widgets) == 1
         for wdg in self._preview_widgets:
             lims = wdg.get_roi()
-            if lims is None: lims = (0,self.detector_shape[0],0,self.detector_shape[1])
+            if lims is None: lims = (0,1,0,1)
         return lims
 
     @property
