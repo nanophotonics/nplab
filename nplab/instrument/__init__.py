a2feb2452a749475c757e337404487aa18544408
YagoDel <yagovalleinclan@gmail.com>
2020-10-15 19:36:01 +0900

    Save instrument configuration to YAML following to a set of given property names

--- nplab/instrument/__init__.py
+++ nplab/instrument/__init__.py
@@ -24,6 +24,8 @@ import inspect
 import os
 import h5py
 import datetime
+import yaml
+import pymsgbox
 LOGGER = create_logger('Instrument')
 LOGGER.setLevel('INFO')
 
@@ -34,6 +36,7 @@ class Instrument(ShowGUIMixin):
     """
     __instances = None
     metadata_property_names = () #"Tuple of names of properties that should be automatically saved as HDF5 metadata
+    config_property_names = ()
 
     def __init__(self):
         """Create an instrument object."""
@@ -178,16 +181,16 @@ class Instrument(ShowGUIMixin):
 
     def open_config_file(self):
         """Open the config file for the current spectrometer and return it, creating if it's not there"""
-        if not hasattr(self,'_config_file'):
+        if not hasattr(self, '_config_file'):
             f = inspect.getfile(self.__class__)
             d = os.path.dirname(f)
-            self._config_file = nplab.datafile.DataFile(h5py.File(os.path.join(d, 'config.h5')))
+            self._config_file = nplab.datafile.DataFile(h5py.File(os.path.join(d, 'config.h5'), 'a'))
             self._config_file.attrs['date'] = datetime.datetime.now().strftime("%H:%M %d/%m/%y")
         return self._config_file
 
     config_file = property(open_config_file)
     
-    def update_config(self, name, data, attrs= None):
+    def update_config(self, name, data, attrs=None):
         """Update the configuration file for this spectrometer.
         
         A file is created in the nplab directory that holds configuration
@@ -201,3 +204,79 @@ class Instrument(ShowGUIMixin):
                 f.flush()    
         else:
             f.create_dataset(name, data=data ,attrs = attrs)
+
+    @property
+    def config(self):
+        """Configuration dictionary
+        Iterates over self.config_property_names and gets the property values
+
+        :return: dictionary
+        """
+        configuration = dict()
+        for name in self.config_property_names:
+            configuration[name] = getattr(self, name)
+        return configuration
+
+    @config.setter
+    def config(self, configuration):
+        for key, value in configuration.items():
+            setattr(self, key, value)
+
+    def _config_filename(self, name=None, extension='.yaml'):
+        """Utility function
+
+        Ensures name is a yaml path, and if it's not an absolute path, it points it to the location of the Python file
+        for the current class
+
+        :param name: string. Can be just the filename, a filename with an extension, or a relative/absolute path
+        :return:
+        """
+        if name is None:
+            name = 'config'  # default name
+        # Ensure name has expected extension
+        root, ext = os.path.splitext(name)
+        if not ext:
+            ext = extension
+        else:
+            assert ext == extension
+        filename = root + ext
+
+        # Default location for YAML is wherever the instance's Python definition is
+        if not os.path.isabs(filename):
+            f = inspect.getfile(self.__class__)
+            d = os.path.dirname(f)
+            filename = os.path.join(d, filename)
+        return filename
+
+    def save_config(self, filename=None):
+        """Saves instrument configuration to YAML
+        :param filename: string
+        """
+        # Get filename
+        filename = self._config_filename(filename)
+
+        # If the file exists, checks whether the user wants to overwrite it
+        if os.path.exists(filename):
+            reply = pymsgbox.prompt(text='That configuration file exists. Do you want to overwrite it?',
+                                    title='', default=filename)
+            if reply is not None:
+                filename = reply
+            else:
+                return
+
+        # Dumps the configuration dictionary to a yaml
+        with open(filename, 'w') as config_file:
+            yaml.dump(self.config, config_file)
+
+    def load_config(self, filename=None):
+        """Loads configuration from YAML
+        :param filename: string
+        :return:
+        """
+        # Get filename
+        filename = self._config_filename(filename)
+
+        # Loads and sets the configuration
+        with open(filename, 'r') as config_file:
+            config = yaml.load(config_file, Loader=yaml.FullLoader)
+        self.config = config
