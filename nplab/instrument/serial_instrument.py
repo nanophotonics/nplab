6eed4da46d95656558980c02a95196b1c5d9bb6b
YagoDel <yagovalleinclan@gmail.com>
2021-03-04 14:46:31 +0900

    Removing lock, since it's handled in the base class

--- nplab/instrument/serial_instrument.py
+++ nplab/instrument/serial_instrument.py
@@ -108,19 +108,17 @@ class SerialInstrument(MessageBusInstrument):
 
     def _write(self,query_string, ignore_echo=False, timeout=None):
         """Write a string to the serial port"""
-        with self.communications_lock:
-            assert self.ser.isOpen(), "Warning: attempted to write to the serial port before it was opened.  Perhaps you need to call the 'open' method first?"
-            try:
-                if self.ser.outWaiting()>0: self.ser.flushOutput() #ensure there's nothing waiting
-            except AttributeError:
-                if self.ser.out_waiting>0: self.ser.flushOutput() #ensure there's nothing waiting
-            if ignore_echo: self.flush_input_buffer()
-            self.ser.write(str.encode(self.initial_character+str(query_string)+self.termination_character))
-            if ignore_echo:
-                echo = self.readline(timeout).strip()
-                if query_string != echo:
-                    self._logger.warn('This write did not echo: ' + echo)
-            # self.ser.write(np.char.encode(np.array([self.initial_character+query_string+self.termination_character]), 'utf8'))
+        assert self.ser.isOpen(), "Warning: attempted to write to the serial port before it was opened.  Perhaps you need to call the 'open' method first?"
+        try:
+            if self.ser.outWaiting()>0: self.ser.flushOutput() #ensure there's nothing waiting
+        except AttributeError:
+            if self.ser.out_waiting>0: self.ser.flushOutput() #ensure there's nothing waiting
+        if ignore_echo: self.flush_input_buffer()
+        self.ser.write(str.encode(self.initial_character+str(query_string)+self.termination_character))
+        if ignore_echo:
+            echo = self.readline(timeout).strip()
+            if query_string != echo:
+                self._logger.warn('This write did not echo: ' + echo)
 
     def flush_input_buffer(self):
         """Make sure there's nothing waiting to be read, and clear the buffer if there is."""
