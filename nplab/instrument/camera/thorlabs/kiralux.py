ef25fca79866b0a46fef824b34626e87d053fb0f
Eoin Elliott <ee306@cam.ac.uk>
2021-05-31 11:02:02 +0100

    kiralux stuff

--- nplab/instrument/camera/thorlabs/kiralux.py
+++ nplab/instrument/camera/thorlabs/kiralux.py
@@ -5,51 +5,72 @@ Created on Fri May 28 15:50:10 2021
 @author: Eoin
 """
 
-from nplab.instrument.camera import Camera
-from thorlabs_tsi_sdk.tl_camera import TLCameraSDK, TLCamera, Frame
+import sys, os, time
+from functools import wraps
+from pathlib import Path
+
+from nplab.instrument.camera import Camera, CameraControlWidget
+from nplab.utils.notified_property import NotifiedProperty
+from nplab.utils.thread_utils import locked_action
+from nplab.ui.ui_tools import QuickControlBox
+
+from thorlabs_tsi_sdk.tl_camera import TLCameraSDK
 from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
 from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
-import sys, os
-from pathlib import Path
-dll_path = Path('dlls')
+
+
+
+dll_path = Path(__file__).parent / 'dlls'
 is_64bits = sys.maxsize > 2**32
 
 if is_64bits:
     dll_path /= '64_lib'
 else:
     dll_path /= '32_lib'
+#this line actually works
 os.environ['PATH'] = str(dll_path.absolute()) + os.pathsep + os.environ['PATH']
 
+# this one should, but doesn't
 os.add_dll_directory(dll_path.absolute())
 
 
-from functools import wraps
-def disarmed(f, self, *args, **kwargs):
+def disarmer(f, wait=0.1):
+    '''some properties like roi and binning and  frames_per_capture.. need the camera to be "disarmed" to
+    be set. This decorator disarms and re-arms the camera before and after the
+    funciton is called. Particularly for binning, it seems like the camera 
+    needs a few secs before it can be changed, hence the time.sleep.
+    '''
     @wraps(f)
-    def inner_func(self):
-        armed = self._camera.isarmed()
+    def inner_func(self, *args, **kwargs):
+        armed = self._camera.is_armed
         if armed:
             self._camera.disarm()
-        out = self.f(*args, **kwargs)
+            time.sleep(wait)
+        out = f(self, *args, **kwargs)
+        self._image_width = self._camera.image_width_pixels
+        self._image_height = self._camera.image_height_pixels # a compromise
+        # between querying for every capture and wrapping binx and biny setter
+        # methods individually. 
         if armed:
-            self._camera.arm(2)
+            self._camera.arm(2) # 2 frames to buffer
+        if self.live_view:
+            self._camera.issue_software_trigger()
         return out
     return inner_func
-        
-        
+             
 class Kiralux(Camera):
+    disarmed_properties = ('roi', 'binx', 'biny', 'frames_per_trigger_zero_for_unlimited')
+    # properties that need the camera to be disarmed to set - there may be more.
+    notified_properties = ('gain',) # properties that are in the gui
     def __init__(self):
-        
+        super().__init__()
         self._sdk = TLCameraSDK()
-        self._sdk.discover_available_cameras()
         self._camera = self._sdk.open_camera(self._sdk.discover_available_cameras()[0])
         
-        
-        self._image_width = self._camera.image_width_pixels
-        self._image_height = self._camera.image_height_pixels
         if self._camera.camera_sensor_type != SENSOR_TYPE.BAYER:
             # Sensor type is not compatible with the color processing library
             self._is_color = False
+            self.process_frame = lambda f: f # no processing for grey images
         else:
             self._mono_to_color_sdk = MonoToColorProcessorSDK()
             self._image_width = self._camera.image_width_pixels
@@ -62,146 +83,110 @@ class Kiralux(Camera):
                 self._camera.bit_depth
             )
             self._is_color = True
+            self.process_frame = self.process_color_frame
 
         self._bit_depth = self._camera.bit_depth
-        self._camera.image_poll_timeout_ms = 0  #
+        self._camera.image_poll_timeout_ms = 0  
+        self._populate_properties()
+        
+        self._camera.frames_per_trigger_zero_for_unlimited = 1 # snapshot mode
         self._camera.arm(2)
+        
+    
+    def _populate_properties(self):
+        ''' adds all the properties from TLCamera to Kiralux, for easy access.
+        
+        '''
+        
+        def prop_factory(thor_prop, disarmed=False, notified=False): # to get around late binding
+            def fget(self):
+                return thor_prop.fget(self._camera)
+            def fset(self, val):
+                return locked_action(thor_prop.fset(self._camera, val))
+            if disarmed: fset = disarmer(fset)
+            if notified: return NotifiedProperty(*map(locked_action, (fget, fset)))
+            return property(*map(locked_action, (fget, fset)))
+        
+        cls = self.__class__    
+        for thor_attr in dir(thor_cls := self._camera.__class__):
+            if hasattr(thor_prop := getattr(thor_cls, thor_attr), 'fget'): 
+                # if it's a property
+                if not hasattr(cls, thor_attr):
+                    # and it's not in Kiralux already
+                    setattr(cls,
+                            thor_attr, # add the property
+                            prop_factory(thor_prop,
+                                         thor_attr in cls.disarmed_properties,
+                                         thor_attr in cls.notified_properties))
+                                         # if it's in disarmed_properties, 
+                                         # decorate the setter and return a 
+                                         # notified property appropriately.
+
+    @NotifiedProperty
+    def exposure(self): # in ms by nplab convention
+        return self.exposure_time_us/1000.0
+    
+    @exposure.setter
+    def exposure(self, val):
+        self.exposure_time_us = int(val*1000)
+     
+    def get_frame(self, timeout=None):
+        '''try to get a frame until it timeouts - shouldn't fail if stuff is
+        working right'''
+        if timeout is None:
+            timeout = (self.exposure/100) # 10*exposure time
+        start = time.time()
+        while not ((f := self._camera.get_pending_frame_or_null())
+                    or (time.time()-start) > timeout):
+            pass
+        return f
+    
+    def process_color_frame(self, frame):
+        color_image_data = self._mono_to_color_processor.transform_to_24(frame.image_buffer,
+                                                                              self._image_width,
+                                                                          self._image_height)
+        color_image_data = color_image_data.reshape(self._image_height, self._image_width, 3)
+        return color_image_data
     
     def raw_snapshot(self):
-        self._camera.issue_software_trigger(1)
-        frame = self._camera.get_pending_frame_or_null()
+        if not self.live_view:
+            self._camera.issue_software_trigger()
+        # if it's in live_view, camera should already be triggered
+        frame = self.get_frame()
         if frame:
-            color_image_data = self._mono_to_color_processor.transform_to_24(frame.image_buffer,
-                                                                             self._image_width,
-                                                                             self._image_height)
-            color_image_data = color_image_data.reshape(self._image_height, self._image_width, 3)
-       
-            return True, color_image_data
+            return True, self.process_frame(frame)
         return False, None
+    
+    @Camera.live_view.setter
+    def live_view(self, live_view):
+        if live_view == self._live_view: return # small redundancy with Camera.live_view
+        Camera.live_view.fset(self, live_view)
+        if live_view:
+            self.frames_per_trigger_zero_for_unlimited = 0 # unlimited
+            # decorator should trigger as self.live_view == True
+        else:
+            self.frames_per_trigger_zero_for_unlimited = 1 # disarms and rearms
+            
+    def get_control_widget(self):
+        "Get a Qt widget with the camera's controls (but no image display)"
+        return KiraluxCameraControlWidget(self)
         
-k = Kiralux()
-k.show_gui(False)
-
-
-
-
-
-# class LiveViewCanvas(tk.Canvas):
-
-#     def __init__(self, parent, image_queue):
-#         # type: (typing.Any, queue.Queue) -> LiveViewCanvas
-#         self.image_queue = image_queue
-#         self._image_width = 0
-#         self._image_height = 0
-#         tk.Canvas.__init__(self, parent)
-#         self.pack()
-#         self._get_image()
-
-#     def _get_image(self):
-#         try:
-#             image = self.image_queue.get_nowait()
-#             self._image = ImageTk.PhotoImage(master=self, image=image)
-#             if (self._image.width() != self._image_width) or (self._image.height() != self._image_height):
-#                 # resize the canvas to match the new image size
-#                 self._image_width = self._image.width()
-#                 self._image_height = self._image.height()
-#                 self.config(width=self._image_width, height=self._image_height)
-#             self.create_image(0, 0, image=self._image, anchor='nw')
-#         except queue.Empty:
-#             pass
-#         self.after(10, self._get_image)
-
-
-""" ImageAcquisitionThread
-
-This class derives from threading.Thread and is given a TLCamera instance during initialization. When started, the 
-thread continuously acquires frames from the camera and converts them to PIL Image objects. These are placed in a 
-queue.Queue object that can be retrieved using get_output_queue(). The thread doesn't do any arming or triggering, 
-so users will still need to setup and control the camera from a different thread. Be sure to call stop() when it is 
-time for the thread to stop.
-
-"""
-
-
-# class ImageAcquisitionThread(threading.Thread):
-
-#     def __init__(self, camera):
-#         # type: (TLCamera) -> ImageAcquisitionThread
-#         super(ImageAcquisitionThread, self).__init__()
-#         self._camera = camera
-#         self._previous_timestamp = 0
-
-#         # setup color processing if necessary
-#         if self._camera.camera_sensor_type != SENSOR_TYPE.BAYER:
-#             # Sensor type is not compatible with the color processing library
-#             self._is_color = False
-#         else:
-#             self._mono_to_color_sdk = MonoToColorProcessorSDK()
-#             self._image_width = self._camera.image_width_pixels
-#             self._image_height = self._camera.image_height_pixels
-#             self._mono_to_color_processor = self._mono_to_color_sdk.create_mono_to_color_processor(
-#                 SENSOR_TYPE.BAYER,
-#                 self._camera.color_filter_array_phase,
-#                 self._camera.get_color_correction_matrix(),
-#                 self._camera.get_default_white_balance_matrix(),
-#                 self._camera.bit_depth
-#             )
-#             self._is_color = True
-
-#         self._bit_depth = camera.bit_depth
-#         self._camera.image_poll_timeout_ms = 0  # Do not want to block for long periods of time
-#         self._image_queue = queue.Queue(maxsize=2)
-#         self._stop_event = threading.Event()
-
-#     def get_output_queue(self):
-#         # type: (type(None)) -> queue.Queue
-#         return self._image_queue
-
-#     def stop(self):
-#         self._stop_event.set()
-
-#     def _get_color_image(self, frame):
-#         # type: (Frame) -> Image
-#         # verify the image size
-#         width = frame.image_buffer.shape[1]
-#         height = frame.image_buffer.shape[0]
-#         if (width != self._image_width) or (height != self._image_height):
-#             self._image_width = width
-#             self._image_height = height
-#             print("Image dimension change detected, image acquisition thread was updated")
-#         # color the image. transform_to_24 will scale to 8 bits per channel
-#         color_image_data = self._mono_to_color_processor.transform_to_24(frame.image_buffer,
-#                                                                          self._image_width,
-#                                                                          self._image_height)
-#         color_image_data = color_image_data.reshape(self._image_height, self._image_width, 3)
-#         # return PIL Image object
-#         return Image.fromarray(color_image_data, mode='RGB')
-
-#     def _get_image(self, frame):
-#         # type: (Frame) -> Image
-#         # no coloring, just scale down image to 8 bpp and place into PIL Image object
-#         scaled_image = frame.image_buffer >> (self._bit_depth - 8)
-#         return Image.fromarray(scaled_image)
-
-#     def run(self):
-#         while not self._stop_event.is_set():
-#             try:
-#                 frame = self._camera.get_pending_frame_or_null()
-#                 if frame is not None:
-#                     if self._is_color:
-#                         pil_image = self._get_color_image(frame)
-#                     else:
-#                         pil_image = self._get_image(frame)
-#                     self._image_queue.put_nowait(pil_image)
-#             except queue.Full:
-#                 # No point in keeping this image around when the queue is full, let's skip to the next one
-#                 pass
-#             except Exception as error:
-#                 print("Encountered error: {error}, image acquisition will stop.".format(error=error))
-#                 break
-#         print("Image acquisition has stopped")
-#         if self._is_color:
-#             self._mono_to_color_processor.dispose()
-#             self._mono_to_color_sdk.dispose()
-
-
+class KiraluxCameraControlWidget(CameraControlWidget):
+    """A control widget for the Thorlabs camera, with extra buttons."""
+    def __init__(self, camera, auto_connect=True):
+        super().__init__(camera, auto_connect=False)
+        gb = QuickControlBox()
+        gb.add_doublespinbox("exposure",
+                             *(e/1000 for e in camera.exposure_time_range_us)) 
+        gb.add_spinbox("gain", *camera.gain_range) # setting range
+        gb.add_button("show_camera_properties_dialog", title="Camera Setup")
+        gb.add_button("show_video_format_dialog", title="Video Format")
+        self.layout().insertWidget(1, gb) # put the extra settings in the middle
+        self.quick_settings_groupbox = gb        
+        
+        self.auto_connect_by_name(controlled_object=self.camera, verbose=False)
+        
+if __name__ == '__main__':        
+                
+    k = Kiralux()
+    k.show_gui(False)
