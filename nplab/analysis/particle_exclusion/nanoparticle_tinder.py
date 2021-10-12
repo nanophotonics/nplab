ae22a05be01051a5b86e611c303e03e6b35d5819
Eoin Elliott <ee306@cam.ac.uk>
2021-10-12 13:34:50 +0100

    Update nanoparticle_tinder.py

--- nplab/analysis/particle_exclusion/nanoparticle_tinder.py
+++ nplab/analysis/particle_exclusion/nanoparticle_tinder.py
@@ -15,73 +15,49 @@ Changing spyder's matplotlib backed to inline makes it faster.
 
 
 """
-from __future__ import print_function
-from __future__ import division
+import time
 
-from builtins import range
-from builtins import str
-from builtins import input
 import matplotlib.pyplot as plt
 import h5py
 import numpy as np
+from tqdm import tqdm
+from nplab.analysis.utils import load_rejected, save_rejected
 
-import time
+
 
 plt.ion()
 
-def accept_reject(group, plot_function, cutoff = 5000):
-    accepted = []
-    rejected = []
-    #---load the previously saved NPs
-    try:
-        
-        previously_accepted = np.load('accepted.npy')
-        previosly_rejected = np.load('rejected.npy')
-    except:
-        print('no previous loaded')
-        previously_accepted = []
-        previosly_rejected = []
-    accepted.extend(previously_accepted)
-    rejected.extend(previosly_rejected)
-    prar = np.append(previously_accepted, previosly_rejected).tolist()
+def reject(group, plot_function, cutoff=5000):
+    group_len = len(next(iter(
+        group.values())))  # number of datasets in each particle group
+    scan = {  # read the scan into a dictionary (but not memory)
+        n: p
+        for n, p in group.items() if ((
+            n.startswith('Particle')  # has the right name
+            and (n not in rejected)  # hasn't been rejected by another script
+            and (len(p) == group_len)))  # is full (nothing went wrong)
+    }
+    scan = {k: scan[k] for k in sorted(scan,
+                                       key=lambda k: int(k.split('_')[-1]))}
     
-    Progress = [10, 25, 50, 75, 90]
-    for index, (p_name, particle) in enumerate(group.items()):
-        
-        if p_name[:3] != 'Par': # discarding non-particle groups
-            continue
-        if int(p_name.split('_')[1]) not in list(range(cutoff)): #eg. if your track stopped after particle 100, put in 101
-            continue
-        if p_name in prar: # if you're continuing from some previously saved accepted/rejected lists
-            continue
-        
-        if Progress and index*100//len(group)>Progress[0] :# prints the progress. May not work if len(group)<100
-            print(str(Progress.pop(0))+'% done')
-            
+    for name, particle in tqdm(scan.items()):
+        if name in rejected: continue
         plot_function(particle)
         plt.pause(0.1)
         
-        ar = input('a/d = accept/decline: ')
+        ar = input('a/d = accept/decline: ').split().lower()
         if ar == 'a':
-            accepted.append(p_name)
-        elif ar == 'd':
-            rejected.append(p_name)
-        if ar == 'v': # stop the program and save the NPs so far
-            np.save('accepted', accepted)
-            np.save('rejected', accepted)
-            break
-        if ar == 'c': # stop the program without saving
-            break
-        plt.close('all') 
-        np.save('accepted', accepted)
-        np.save('rejected', accepted)
+            pass
+        if ar == 'd':
+            rejected.add(name)
     
-    return accepted, rejected
+    return rejected
             
 
 if __name__ == '__main__':
     plt.rc('font',family='arial', size=18)
     start = time.time()
+    rejected = load_rejected()
     
     def plot_function(particle):
         fig, ax = plt.subplots(1, 3, figsize=(30, 10)) #3 subplots, feel free to use more
@@ -94,6 +70,5 @@ if __name__ == '__main__':
        
         
     with 'Your_File_here' as File:   
-        accepted, rejected = accept_reject(File['ParticleScannerScan_0'])    
-        np.save('accepted', accepted)
-        np.save('rejected', accepted)
+        rejected = accept_reject(File['ParticleScannerScan_0'], plot_function, rejected)    
+        save_rejected(rejected)
