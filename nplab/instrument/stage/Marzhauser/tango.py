26641c519649bc1eef0a368444f27326c0cad168
James Stevenson <mooseling@hotmail.com>
2021-12-29 18:28:10 +0000

    [Tango] Import DLL

--- nplab/instrument/stage/Marzhauser/tango.py
+++ nplab/instrument/stage/Marzhauser/tango.py
@@ -1,5 +1,21 @@
 from nplab.instrument import Instrument
 from nplab.instrument.stage import Stage
+import ctypes
+import sys
+
+# Load the Tango DLL
+system_bits = '64' if (sys.maxsize > 2**32) else '32'
+tango_dll = ctypes.cdll.LoadLibrary(f'DLL/{system_bits}/Tango_DLL')
+
+# Set arg types for all dll functions we call
+# LSID: Used to tell the DLL which Tango we are sending a command to
+# The DLL can have up to 8 simultaneously connected Tangos
+tango_dll.LSX_CreateLSID.argtypes = [ctypes.POINTER(ctypes.c_int)]
+tango_dll.LSX_ConnectSimple.argtypes = [ctypes.c_int, ctypes.c_int,
+                                        ctypes.POINTER(ctypes.c_char),
+                                        ctypes.c_int, ctypes.c_bool]
+tango_dll.LSX_Disconnect.argtypes = [ctypes.c_int]
+tango_dll.LSX_FreeLSID.argtypes = [ctypes.c_int]
 
 
 class Tango(Stage):
