7fdf7bbf170670f02c0cc711046a5746a0b58ee6
James Stevenson <mooseling@hotmail.com>
2021-12-31 11:27:14 +0000

    [Tango] Merge DLL wrapper with main class

--- nplab/instrument/stage/Marzhauser/tango.py
+++ nplab/instrument/stage/Marzhauser/tango.py
@@ -18,30 +18,6 @@ tango_dll.LSX_Disconnect.argtypes = [ctypes.c_int]
 tango_dll.LSX_FreeLSID.argtypes = [ctypes.c_int]
 
 
-class TangoWrapper():
-    # Every time we call functions in the DLL, we want to check for errors
-    # To simplify the Tango class, we move all that boilerplate into TangoWrapper
-    def CreateLSID(self, lsid_ref):
-        return_value = tango_dll.LSX_CreateLSID(lsid_ref)
-        assert return_value != 0, f'Tango.LSX_CreateLSID returned {return_value}'
-
-    def ConnectSimple(self, lsid, interface_type, com_name, baud_rate, show_protocol):
-        return_value = tango_dll.LSX_ConnectSimple(lsid, interface_type, com_name, baud_rate,
-                                                   show_protocol)
-        assert return_value != 0, f'Tango.LSX_ConnectSimple returned {return_value}'
-
-    def Disconnect(self, lsid):
-        return_value = tango_dll.LSX_Disconnect(lsid)
-        assert return_value != 0, f'Tango.LSX_Disconnect returned {return_value}'
-
-    def FreeLSID(self, lsid):
-        return_value = tango_dll.LSX_FreeLSID(lsid)
-        assert return_value != 0, f'Tango.LSX_FreeLSID returned {return_value}'
-
-
-tango_wrapper = TangoWrapper()
-
-
 class Tango(Stage):
     def __init__(self, unit='m'):
         Instrument.__init__(self)
@@ -49,13 +25,13 @@ class Tango(Stage):
 
         # Connect to Tango
         self.lsid = ctypes.c_int()
-        tango_wrapper.CreateLSID(ctypes.byref(self.lsid))
-        tango_wrapper.ConnectSimple(self.lsid, ctypes.c_int(-1), None, ctypes.c_int(57600),
-                                    ctypes.c_bool(False))
+        Tango.CreateLSID(ctypes.byref(self.lsid))
+        Tango.ConnectSimple(self.lsid, ctypes.c_int(-1), None, ctypes.c_int(57600),
+                            ctypes.c_bool(False))
 
     def close(self):
-        tango_wrapper.Disconnect(self.lsid)
-        tango_wrapper.FreeLSID(self.lsid)
+        Tango.Disconnect(self.lsid)
+        Tango.FreeLSID(self.lsid)
 
     def move(self, pos, axis=None, relative=False):
         raise NotImplementedError("You must override move() in a Stage subclass")
@@ -66,3 +42,29 @@ class Tango(Stage):
     def is_moving(self, axes=None):
         """Returns True if any of the specified axes are in motion."""
         raise NotImplementedError("The is_moving method must be subclassed and implemented before it's any use!")
+
+    @staticmethod
+    def CreateLSID(lsid_ref):
+        return_value = tango_dll.LSX_CreateLSID(lsid_ref)
+        assert return_value != 0, f'Tango.LSX_CreateLSID returned {return_value}'
+
+    @staticmethod
+    def ConnectSimple(lsid, interface_type, com_name, baud_rate, show_protocol):
+        return_value = tango_dll.LSX_ConnectSimple(lsid, interface_type, com_name, baud_rate,
+                                                   show_protocol)
+        assert return_value != 0, f'Tango.LSX_ConnectSimple returned {return_value}'
+
+    @staticmethod
+    def Disconnect(lsid):
+        return_value = tango_dll.LSX_Disconnect(lsid)
+        assert return_value != 0, f'Tango.LSX_Disconnect returned {return_value}'
+
+    @staticmethod
+    def FreeLSID(lsid):
+        return_value = tango_dll.LSX_FreeLSID(lsid)
+        assert return_value != 0, f'Tango.LSX_FreeLSID returned {return_value}'
+
+    @staticmethod
+    def SetDimensions(lsid, x_dim, y_dim, z_dim, a_dim):
+        return_value = tango_dll.LSX_SetDimensions(lsid, x_dim, y_dim, z_dim, a_dim)
+        assert return_value != 0, f'Tango.LSX_SetDimensions returned {return_value}'
