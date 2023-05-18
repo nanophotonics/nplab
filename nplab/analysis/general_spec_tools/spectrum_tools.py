4de53cb71db2de16f8a5ff6d997ea6a287b2a7eb
il322 <il322@cam.ac.uk>
2023-05-18 13:55:10 +0100

    Fixed timescan truncation
    Needed to re-define self.y (avg of timescans) after truncation

--- nplab/analysis/general_spec_tools/spectrum_tools.py
+++ nplab/analysis/general_spec_tools/spectrum_tools.py
@@ -986,3 +986,7 @@ class Timescan(Spectrum):
             else:
                 plt.close('all')
             plt.rcParams.update(old_rc_params)#put rcParams back to normal when done
+            
+    def truncate(self, start_x, end_x, buffer=False):
+        self.x, self.Y = truncate_spectrum(self.x, self.Y, start_wl = start_x, end_wl = end_x, buffer=False)
+        self.y = np.average(self.Y, axis = 0)
